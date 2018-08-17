a7166dd4178abfaf210b7b2b068b1c999b6e2004
Jan Nils Ferner <jan@myelin.ch>
2018-08-17 12:06:08 +0200

    Add basic traits

--- environment/src/lib.rs
+++ environment/src/lib.rs
@@ -1,4 +1,12 @@
-pub fn foo() {}
+pub trait Locatable {
+    fn x() -> u32;
+    fn y() -> u32;
+}
+
+pub trait Rectangle {
+    fn height() -> u32;
+    fn width() -> u32;
+}
 
 #[cfg(test)]
 mod tests {
