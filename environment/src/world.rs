1d5fc898d94f85fdbe2f05a61a16f039aac4cc59
Jan Nils Ferner <jan@myelin.ch>
2018-08-17 15:56:57 +0200

    Add newlines

--- environment/src/world.rs
+++ environment/src/world.rs
@@ -16,6 +16,7 @@ impl Rectangle for WorldImpl {
     fn width(&self) -> u32 {
         unimplemented!()
     }
+
     fn length(&self) -> u32 {
         unimplemented!()
     }
@@ -25,12 +26,15 @@ impl World for WorldImpl {
     fn collidables() -> Vec<Box<Collidable>> {
         unimplemented!()
     }
+
     fn add_collidable(collidable: Box<Collidable>) -> usize {
         unimplemented!()
     }
+
     fn remove_collidable(collidable: usize) {
         unimplemented!()
     }
+
     fn update_collidable(id: usize, collidable: Box<Collidable>) {
         unimplemented!()
     }
