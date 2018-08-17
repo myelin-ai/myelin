dc0afabc11c620938c67c3a8da455737f4df695e
Jan Nils Ferner <jan@myelin.ch>
2018-08-17 12:55:03 +0200

    Add world CRUD

--- environment/src/world.rs
+++ environment/src/world.rs
@@ -2,6 +2,9 @@ use super::traits::*;
 
 pub trait World: Rectangle {
     fn collidables() -> Vec<Box<Collidable>>;
+    fn add_collidable(collidable: Box<Collidable>) -> usize;
+    fn remove_collidable(collidable: usize);
+    fn update_collidable(id: usize, collidable: Box<Collidable>);
 }
 
 pub struct WorldImpl {
@@ -21,5 +24,22 @@ impl Rectangle for WorldImpl {
 impl World for WorldImpl {
     fn collidables() -> Vec<Box<Collidable>> {
         unimplemented!()
+    }
+    fn add_collidable(collidable: Box<Collidable>) -> usize {
+        unimplemented!()
+    }
+    fn remove_collidable(collidable: usize) {
+        unimplemented!()
+    }
+    fn update_collidable(id: usize, collidable: Box<Collidable>) {
+        unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(true, true);
     }
 }
