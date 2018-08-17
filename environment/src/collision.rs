bde61976d0cd354d3c147073ec858e7599710f4c
Jan Nils Ferner <jan@myelin.ch>
2018-08-17 16:51:13 +0200

    Add basic quadtree skeleton

--- environment/src/collision.rs
+++ environment/src/collision.rs
@@ -1,3 +1,30 @@
-use traits::CollidableContainer;
+use traits::*;
 
 pub trait CollisionHandler: CollidableContainer {}
+
+pub struct QuadTree {}
+
+impl CollidableContainer for QuadTree {
+    fn collidables() -> Vec<Box<Collidable>> {
+        unimplemented!()
+    }
+    fn add_collidable(_collidable: Box<Collidable>) -> usize {
+        unimplemented!()
+    }
+    fn remove_collidable(_collidable: usize) {
+        unimplemented!()
+    }
+    fn update_collidable(_id: usize, _collidable: Box<Collidable>) {
+        unimplemented!()
+    }
+}
+
+impl CollisionHandler for QuadTree {}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!("quad_tree", "quad_tree")
+    }
+}
