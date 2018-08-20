7061a367cce63268df022ab5d0b5cf5fa103e8cd
Jan Nils Ferner <jan@myelin.ch>
2018-08-20 16:31:13 +0200

    Strongly type ids

--- environment/src/collidable_container.rs
+++ environment/src/collidable_container.rs
@@ -1,8 +1,10 @@
 use crate::properties::Collidable;
 
+pub type CollidableId = usize;
+
 pub trait CollidableContainer {
     fn collidables(&self) -> Vec<Box<dyn Collidable>>;
-    fn add_collidable(&mut self, collidable: Box<dyn Collidable>) -> usize;
-    fn remove_collidable(&mut self, collidable: usize);
-    fn update_collidable(&mut self, id: usize, collidable: Box<dyn Collidable>);
+    fn add_collidable(&mut self, collidable: Box<dyn Collidable>) -> CollidableId;
+    fn remove_collidable(&mut self, id: CollidableId);
+    fn update_collidable(&mut self, id: CollidableId, collidable: Box<dyn Collidable>);
 }
--- environment/src/properties.rs
+++ environment/src/properties.rs
@@ -1,3 +1,5 @@
+use crate::collidable_container::CollidableId;
+
 pub trait Locatable {
     fn x(&self) -> u32;
     fn y(&self) -> u32;
@@ -14,16 +16,11 @@ pub struct Vector {
 }
 
 pub enum SpawnEvent {
-    Spawn(Box<dyn Collidable>),
-    Despawn(usize),
-    Move(usize, Vector),
+    Spawn(Box<dyn Fn(CollidableId) -> Box<dyn Collidable>>),
+    Despawn(CollidableId),
+    Move(CollidableId, Vector),
 }
 
 pub trait Collidable: Locatable + Rectangle {
-    fn id(&self) -> usize;
-    fn on_collision(
-        &self,
-        other: &dyn Collidable,
-        movement: Option<Vector>,
-    ) -> Vec<SpawnEvent>;
+    fn id(&self) -> CollidableId;
 }
