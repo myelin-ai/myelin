307cd57338035fccc91f8e59976f9841f7235020
Jan Nils Ferner <jan@myelin.ch>
2018-08-24 14:36:15 +0200

    Refactor physics components

--- environment/src/lib.rs
+++ environment/src/lib.rs
@@ -1,7 +1,7 @@
 #![deny(rust_2018_idioms, missing_debug_implementations)]
 
 pub mod collision_detector;
-pub mod collision_handler;
 pub mod object;
+pub mod physics_handler;
 pub mod properties;
 pub mod world;
--- environment/src/properties.rs
+++ environment/src/properties.rs
@@ -1,33 +1,42 @@
 use std::fmt::Debug;
 
-pub trait Locatable {
-    fn x(&self) -> u32;
-    fn y(&self) -> u32;
-}
-
-pub trait Rectangle {
-    fn length(&self) -> u32;
-    fn width(&self) -> u32;
+#[derive(Debug)]
+pub struct Location {
+    x: u32,
+    y: u32,
 }
 
 #[derive(Debug)]
-pub struct Vector {
+pub struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+#[derive(Debug)]
+pub struct MovementVector {
     pub x: i32,
     pub y: i32,
 }
 
 pub type Id = usize;
 
-pub trait Identifiable {
-    fn id(&self) -> Id;
-}
-
 #[derive(Debug)]
 pub enum Kind {
     Organism,
     Wall,
+    Plant,
+    Water,
 }
 
-pub trait Object: Locatable + Rectangle + Identifiable + Debug {
+pub trait Object: Debug {
+    fn location(&self) -> Location;
+    fn set_location(&mut self, location: Location);
+
+    fn rectangle(&self) -> Rectangle;
+    fn set_rectangle(&mut self, rectangle: Rectangle);
+
+    fn movement_vector(&self) -> MovementVector;
+    fn set_movement_vector(&mut self, movement_vector: MovementVector);
+
     fn kind(&self) -> Kind;
 }
