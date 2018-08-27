7318028ea4bb77b650206c51cd548baa87a9727e
Jan Nils Ferner <jan@myelin.ch>
2018-08-27 16:34:41 +0200

    Add basic canvas drawing from rust

--- visualization/src/main.ts
+++ visualization/src/main.ts
@@ -1,2 +1,6 @@
 const rust = import('../out/myelin_visualization')
-rust.then(rust => { rust.greet("World!"); })
+rust.then(rust => {
+    const canvas = document.getElementById('myelin-visualization') as HTMLCanvasElement
+    const entryPoint = rust.EntryPoint.new(canvas)
+    entryPoint.start()
+})
