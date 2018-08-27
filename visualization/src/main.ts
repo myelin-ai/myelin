111c7d7b096b17dcfe5313456e41863ad8fc0154
Jan Nils Ferner <jan@myelin.ch>
2018-08-27 18:08:50 +0200

    Set TSLint to standard JS + trailing comma

--- visualization/src/main.ts
+++ visualization/src/main.ts
@@ -1,6 +1,11 @@
-const rust = import('../out/myelin_visualization')
-rust.then(rust => {
+import('../out/myelin_visualization').then((rust) => {
     const canvas = document.getElementById('myelin-visualization') as HTMLCanvasElement
     const entryPoint = rust.EntryPoint.new(canvas)
     entryPoint.start()
+}).catch((reason) => {
+    console.error(reason)
+    document.body.appendChild(document.createTextNode('Failed to load WASM'))
+    const reasonElement = document.createElement('pre')
+    reasonElement.innerText = reason
+    document.body.appendChild(reasonElement)
 })
