77d03972b3daa0510954062b83742a742f17c590
Jan Nils Ferner <jan@myelin.ch>
2018-08-27 10:58:10 +0200

    Add hello world code

--- visualization/src/lib.rs
+++ visualization/src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
 }
