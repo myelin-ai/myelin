8abb5630fb3a65d4ade7d078e7963425b9a69a5a
Jan Nils Ferner <jan@myelin.ch>
2018-08-27 13:51:08 +0200

    Refactor script

--- visualization/scripts/build.sh
+++ visualization/scripts/build.sh
@@ -4,11 +4,11 @@ set -e
 application_name=myelin_visualization
 target_dir=visualization/out
 
-(cd visualization && yarn)
 
 cargo build --target wasm32-unknown-unknown
+
 rm -r $target_dir || true
 mkdir $target_dir
 wasm-bindgen target/wasm32-unknown-unknown/debug/$application_name.wasm --out-dir $target_dir
 
-(cd visualization && yarn webpack)
+(cd visualization && yarn && yarn webpack)
