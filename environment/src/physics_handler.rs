d9925b7d71f6c583e9207108f831d9359bf184e0
Jan Nils Ferner <jan@myelin.ch>
2018-08-31 10:50:48 +0200

    Use slablit in env

--- environment/src/physics_handler.rs
+++ environment/src/physics_handler.rs
@@ -1,6 +1,7 @@
 use crate::collision_detector::CollisionIter;
 use crate::properties::Object;
 use slab::Slab;
+use myelin_slablit::slab;
 
 pub trait PhysicsHandler {
     fn handle_collisions<'a>(
@@ -12,10 +13,36 @@ pub trait PhysicsHandler {
     fn apply_movement(container: &mut Slab<Object>);
 }
 
+struct PhysicsHandlerImpl;
+
+impl PhysicsHandlerImpl {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl PhysicsHandler for PhysicsHandlerImpl {
+    fn handle_collisions<'a>(
+        &self,
+        collisions: Box<CollisionIter<'a>>,
+        container: &mut Slab<Object>,
+    ) {
+        unimplemented!()
+    }
+
+    fn apply_movement(container: &mut Slab<Object>) {
+        unimplemented!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    fn test_container() -> Slab<Object> {
+
+    }
+
     #[test]
-    fn it_works() {
+    fn applies_movement_to_all_objects() {
         assert_eq!("quad_tree", "quad_tree")
     }
 }
