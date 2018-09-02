889cf1b39a9f4ef64134b677352a27932faf63c6
Jan Nils Ferner <jan@myelin.ch>
2018-09-02 22:33:52 +0200

    Add basic unit tests

--- environment/src/physics_handler/physics_handler_facade/collision_handler_impl.rs
+++ environment/src/physics_handler/physics_handler_facade/collision_handler_impl.rs
@@ -18,6 +18,53 @@ impl CollisionHandler for CollisionHandlerImpl {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use crate::physics_handler::{CollisionIterMut, CollisionMut};
+    use crate::properties::{Kind, Location, MovementVector, Object, Rectangle};
+
     #[test]
-    fn ignores_empty_iterator() {}
+    fn ignores_empty_iterator() {
+        let iter = CollisionIterMut(Box::new(vec![].into_iter()));
+        let collision_handler = CollisionHandlerImpl::new();
+        collision_handler.handle_collisions(iter);
+    }
+
+    #[test]
+    fn ignores_incorrect_collisions() {
+        let original_container = vec![
+            Object {
+                rectangle: Rectangle {
+                    width: 3,
+                    length: 4,
+                },
+                location: Location { x: 17, y: 23 },
+                movement: MovementVector { x: -4, y: -3 },
+                kind: Kind::Undefined,
+            },
+            Object {
+                rectangle: Rectangle {
+                    width: 4,
+                    length: 4,
+                },
+                location: Location { x: 42, y: 11 },
+                movement: MovementVector { x: 0, y: -3 },
+                kind: Kind::Undefined,
+            },
+        ];
+        let mut container = original_container.clone();
+        let (container_a, container_b) = container.split_at_mut(1);
+        {
+            let collisions = vec![CollisionMut {
+                first: &mut container_a[0],
+                second: &mut container_b[0],
+            }];
+            let iter = CollisionIterMut(Box::new(collisions.into_iter()));
+            let collision_handler = CollisionHandlerImpl::new();
+            collision_handler.handle_collisions(iter);
+        }
+        original_container
+            .into_iter()
+            .zip(container)
+            .for_each(|(expected, actual)| assert_eq!(expected, actual));
+    }
 }
