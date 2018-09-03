cebed60fa99acea420df0abe5a515b06532757ea
Jeremy Stucki <jeremy@myelin.ch>
2018-09-03 08:54:06 +0200

    Make rustfmt happy

--- environment/src/physics_handler.rs
+++ environment/src/physics_handler.rs
@@ -2,11 +2,6 @@ use crate::collision_detector::CollisionIter;
 use crate::properties::Object;
 
 pub trait PhysicsHandler {
-    fn handle_collisions<'a>(
-        &self,
-        collisions: Box<CollisionIter<'a>>,
-        container: &mut [Object],
-    );
-
+    fn handle_collisions<'a>(&self, collisions: Box<CollisionIter<'a>>, container: &mut [Object]);
     fn apply_movement(&self, container: &mut [Object]);
 }
