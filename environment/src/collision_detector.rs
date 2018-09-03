9fbef2b464b4493464c2c8cad594e6cef35973e4
Ruben Schmidmeister <ruben@myelin.ch>
2018-09-03 19:19:16 +0200

    Update traits for new object movement (#36)

--- environment/src/collision_detector.rs
+++ environment/src/collision_detector.rs
@@ -1,18 +1,13 @@
-use crate::properties::Object;
+use crate::properties::{Object, ObjectId};
 use std::fmt::Debug;
 
-pub trait CollisionDetector: Debug {
-    fn are_colliding(&self, first: &Object, second: &Object) -> bool;
+pub trait ObjectContainer: Debug + CollisionChecker {
+    fn add(&mut self, object: Object) -> ObjectId;
+    fn remove(&mut self, id: ObjectId) -> Option<Object>;
+    fn update(&mut self, id: ObjectId, object: Object) -> Option<Object>;
+    fn get(&self, id: ObjectId) -> Option<&Object>;
 }
 
-#[derive(Debug)]
-pub struct Collision<'a> {
-    pub first: &'a Object,
-    pub second: &'a Object,
-}
-
-pub type CollisionIter<'a> = dyn Iterator<Item = Collision<'a>>;
-
-pub trait CollisionGatherer {
-    fn gather_collisions<'a>(&self, container: &'a [Object]) -> Box<CollisionIter<'a>>;
+pub trait CollisionChecker {
+    fn get_collisions(&self, id: ObjectId) -> Vec<&Object>;
 }
--- environment/src/physics_handler.rs
+++ environment/src/physics_handler.rs
@@ -1,7 +1,6 @@
-use crate::collision_detector::CollisionIter;
 use crate::properties::Object;
 
 pub trait PhysicsHandler {
-    fn handle_collisions<'a>(&self, collisions: Box<CollisionIter<'a>>, container: &mut [Object]);
-    fn apply_movement(&self, container: &mut [Object]);
+    fn handle_collisions<'a>(&self, object: &Object, collisions: &[&Object]) -> Object;
+    fn apply_movement(&self, object: &Object) -> Object;
 }
