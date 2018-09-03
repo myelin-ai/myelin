4eb679cbdf0da8cac409a0fa8fd08ea6ae2740d6
Jan Nils Ferner <jan@myelin.ch>
2018-09-03 19:24:47 +0200

    Rename after recent refactoring

--- environment/src/object_container.rs
+++ environment/src/object_container.rs
@@ -1,13 +1,13 @@
 use crate::properties::{Object, ObjectId};
 use std::fmt::Debug;
 
+pub trait CollisionChecker {
+    fn get_collisions(&self, id: ObjectId) -> Vec<&Object>;
+}
+
 pub trait ObjectContainer: Debug + CollisionChecker {
     fn add(&mut self, object: Object) -> ObjectId;
     fn remove(&mut self, id: ObjectId) -> Option<Object>;
     fn update(&mut self, id: ObjectId, object: Object) -> Option<Object>;
     fn get(&self, id: ObjectId) -> Option<&Object>;
-}
-
-pub trait CollisionChecker {
-    fn get_collisions(&self, id: ObjectId) -> Vec<&Object>;
 }
