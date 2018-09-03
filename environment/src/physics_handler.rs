a1a77d864031a1dcb509367d9348b8315f040e69
Ruben Schmidmeister <ruben@myelin.ch>
2018-09-03 19:20:36 +0200

    Remove unused lifetime

--- environment/src/physics_handler.rs
+++ environment/src/physics_handler.rs
@@ -1,6 +1,6 @@
 use crate::properties::Object;
 
 pub trait PhysicsHandler {
-    fn handle_collisions<'a>(&self, object: &Object, collisions: &[&Object]) -> Object;
+    fn handle_collisions(&self, object: &Object, collisions: &[&Object]) -> Object;
     fn apply_movement(&self, object: &Object) -> Object;
 }
