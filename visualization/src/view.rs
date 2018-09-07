3075776aa0b77a0a6ea35c4d74d2f79268bddec5
Jeremy Stucki <jeremy@myelin.ch>
2018-09-07 15:38:55 +0200

    Implement draw_object

--- visualization/src/view.rs
+++ visualization/src/view.rs
@@ -1,6 +1,6 @@
 use crate::js;
 use crate::presenter::View;
-use crate::view_model::{Object, ViewModel};
+use crate::view_model::{Object, ViewModel, Kind};
 
 pub(crate) struct CanvasView {
     context: js::CanvasRenderingContext2D,
@@ -21,8 +21,22 @@ impl CanvasView {
         }
     }
 
-    fn draw_object(&self, _object: &Object) {
-        self.context.set_fill_style("aquamarine");
-        self.context.fill_rect(0, 0, 20, 20);
+    fn draw_object(&self, object: &Object) {
+        self.context.move_to(object.body.vertices[0].x, object.body.vertices[0].y);
+        self.context.begin_path();
+
+        for vertex in &object.body.vertices[1..] {
+            self.context.move_to(vertex.x, vertex.y);
+        }
+
+        let color = match object.kind {
+            Kind::Organism => "orange",
+            Kind::Plant => "green",
+            Kind::Water => "blue",
+            Kind::Terrain => "brown",
+        };
+
+        self.context.set_fill_style(color);
+        self.context.fill();
     }
 }
