e6c06e9ec9173114c93b5ab91ead266951088635
Jan Nils Ferner <jan@myelin.ch>
2018-09-10 14:08:45 +0200

    Remove hardcoded values

--- environment/src/world.rs
+++ environment/src/world.rs
@@ -23,29 +23,9 @@ pub struct WorldImpl {
 
 impl WorldImpl {
     pub fn new() -> Self {
-        let mut physics_world = PhysicsWorld::new();
-        let mut bodies = Vec::new();
-        let cuboid = ShapeHandle::new(Cuboid::new(Vector::new(1.0, 2.0)));
-        let local_inertia = cuboid.inertia(0.1);
-        let local_center_of_mass = cuboid.center_of_mass();
-        let rigid_body_handle = physics_world.add_rigid_body(
-            Isometry::new(Vector::new(2.0, 10.0), na::zero()),
-            local_inertia,
-            local_center_of_mass,
-        );
-        bodies.push(rigid_body_handle);
-
-        let material = Material::default();
-        let _collider_handle = physics_world.add_collider(
-            0.04,
-            cuboid,
-            rigid_body_handle,
-            Isometry::identity(),
-            material,
-        );
         Self {
-            physics_world,
-            bodies,
+            physics_world: PhysicsWorld::new(),
+            bodies: Vec::new(),
         }
     }
 }
