ba964660433498b99809730adf63d4a94d8b000c
Ruben Schmidmeister <ruben@myelin.ch>
2018-09-10 17:47:49 +0200

    Use builder in world example

--- environment/examples/world.rs
+++ environment/examples/world.rs
@@ -1,23 +1,26 @@
-use myelin_environment::object::{Kind, Location, Object, Polygon, Radians, Velocity, Vertex};
+use myelin_environment::object::Kind;
+use myelin_environment::object_builder::{ObjectBuilder, PolygonBuilder};
 use myelin_environment::world::{World, WorldImpl};
 
 fn main() {
     let mut world = WorldImpl::new();
 
-    world.add_object(Object {
-        orientation: Radians(0.0),
-        shape: Polygon {
-            vertices: vec![
-                Vertex { x: 0, y: 0 },
-                Vertex { x: 10, y: 0 },
-                Vertex { x: 0, y: 10 },
-                Vertex { x: 10, y: 10 },
-            ],
-        },
-        location: Location { x: 30, y: 40 },
-        velocity: Velocity { x: 4, y: 5 },
-        kind: Kind::Organism,
-    });
+    world.add_object(
+        ObjectBuilder::default()
+            .shape(
+                PolygonBuilder::default()
+                    .vertex(0, 0)
+                    .vertex(10, 0)
+                    .vertex(0, 10)
+                    .vertex(10, 10)
+                    .build()
+                    .unwrap(),
+            ).location(30, 40)
+            .velocity(4, 5)
+            .kind(Kind::Organism)
+            .build()
+            .unwrap(),
+    );
 
     println!("{:#?}", world);
 }
