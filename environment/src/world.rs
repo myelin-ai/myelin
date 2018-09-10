947639d7e247ac149019e8866867583e228d14a7
Jeremy Stucki <jeremy@myelin.ch>
2018-09-10 15:00:17 +0200

    Move from body_handles to collider_handles

--- environment/src/world.rs
+++ environment/src/world.rs
@@ -1,6 +1,6 @@
 use ncollide2d::shape::{ConvexPolygon, ShapeHandle};
 use nphysics2d::math::{Isometry, Point, Vector};
-use nphysics2d::object::BodyHandle;
+use nphysics2d::object::ColliderHandle;
 use nphysics2d::object::Material;
 use nphysics2d::volumetric::Volumetric;
 use nphysics2d::world::World as PhysicsWorld;
@@ -19,14 +19,14 @@ pub trait World: fmt::Debug {
 #[derive(Default)]
 pub struct WorldImpl {
     physics_world: PhysicsWorld<f64>,
-    body_handles: Vec<BodyHandle>,
+    collider_handles: Vec<ColliderHandle>,
 }
 
 impl WorldImpl {
     pub fn new() -> Self {
         Self {
             physics_world: PhysicsWorld::new(),
-            body_handles: Vec::new(),
+            collider_handles: Vec::new(),
         }
     }
 }
@@ -34,11 +34,12 @@ impl WorldImpl {
 impl World for WorldImpl {
     fn step(&mut self) {
         self.physics_world.step();
-        for body_handle in &self.body_handles {
-            let rigid_body = self
-                .physics_world
-                .rigid_body(*body_handle)
-                .expect("Attempted to access invalid rigid body handle");
+
+        for collider_handle in &self.collider_handles {
+            let collider = self.physics_world.collider(*collider_handle).expect("Attempted to access invalid collider handle");
+            let rigid_body_handle = collider.data().body();
+            let rigid_body = self.physics_world.rigid_body(rigid_body_handle).expect("Attempted to access invalid rigid body handle");
+
             print!("{}", rigid_body.position());
         }
     }
@@ -65,13 +66,15 @@ impl World for WorldImpl {
         );
 
         let material = Material::default();
-        let _collider_handle = self.physics_world.add_collider(
+        let collider_handle = self.physics_world.add_collider(
             0.04,
             shape,
             rigid_body_handle,
             Isometry::identity(),
             material,
         );
+
+        self.collider_handles.push(collider_handle);
     }
 
     fn objects(&self) -> Vec<Object> {
@@ -82,7 +85,7 @@ impl World for WorldImpl {
 impl fmt::Debug for WorldImpl {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("WorldImpl")
-            .field("body_handles", &self.body_handles)
+            .field("collider_handles", &self.collider_handles)
             .field("physics", &DebugPhysicsWorld(&self.physics_world))
             .finish()
     }
