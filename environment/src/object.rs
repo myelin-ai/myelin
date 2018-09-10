a59f567bdf882d3538f82b0d75b4cac9ecf59a0f
Jan Nils Ferner <jan@myelin.ch>
2018-09-10 14:08:30 +0200

    Add velocity to business object

--- environment/src/object.rs
+++ environment/src/object.rs
@@ -1,6 +1,7 @@
 #[derive(Debug)]
 pub struct Object {
     pub body: Polygon,
+    pub velocity: Velocity,
     pub kind: Kind,
 }
 
@@ -13,6 +14,12 @@ pub struct Polygon {
 pub struct Vertex {
     pub x: u32,
     pub y: u32,
+}
+
+#[derive(Debug)]
+pub struct Velocity {
+    pub x: i32,
+    pub y: i32,
 }
 
 #[derive(Debug)]
