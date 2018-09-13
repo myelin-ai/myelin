8ae4caf310e882a10401cde5dd1084dd6e16eceb
Jan Nils Ferner <jan@myelin.ch>
2018-09-13 21:10:44 +0200

    Revert "Fix webpack issues by using a workaround"
    This reverts commit 9cf6e0417280501fd2e928d037b8e4f06f97fb2c.

--- visualization/webpack.config.js
+++ visualization/webpack.config.js
@@ -2,25 +2,14 @@ const path = require('path');
 
 module.exports = {
   entry: "./src/main.ts",
-  node: false,
-  mode: "development",
   module: {
     rules: [{
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: 'ts-loader',
-      },
-      {
-        test: /\.wasm$/,
-        exclude: /node_modules/,
-        type: "javascript/auto",
-        loader: "file-loader",
-        options: {
-          publicPath: "dist/"
-        }
-      }
-    ]
+      test: /\.ts$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }]
   },
+  node: false,
   output: {
     publicPath: '/dist/',
     path: path.resolve(__dirname, "dist"),
@@ -29,4 +18,5 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.js', '.wasm'],
   },
+  mode: "development",
 };
