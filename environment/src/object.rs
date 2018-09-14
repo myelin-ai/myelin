dbfc8ddcd3f9928ed81810097b2e6aafeb4943b9
Jan Nils Ferner <jan@myelin.ch>
2018-09-14 15:32:31 +0200

    Start documenting objects for #61

--- environment/src/object.rs
+++ environment/src/object.rs
@@ -1,9 +1,47 @@
+//! This module containes all objects that can be
+//! placed in a world and their components.
+
+/// An objects that can be placed in the world.
+/// Its coordinates are relative to the center of
+/// the object, which is determined by the `location`
+/// The following would define a stationary square terrain:
+/// ```
+///use myelin_environment::object::*;
+///
+///let square = LocalObject {
+///    shape: LocalPolygon {
+///        vertices: vec![
+///            LocalVertex { x: -50, y: -50 },
+///            LocalVertex { x: -50, y: 50 },
+///            LocalVertex { x: 50, y: 50 },
+///            LocalVertex { x: 50, y: -50 },
+///        ],
+///    },
+///    orientation: Radians(0.0),
+///    location: Location { x: 100, y: 100 },
+///    velocity: Velocity { x: 0, y: 0 },
+///    kind: Kind::Terrain,
+///};
+/// ```
+/// The prefered way of constructing these however
+/// is by using an ObjectBuilder
 #[derive(Debug, PartialEq, Clone)]
 pub struct LocalObject {
+    /// The vertices defining the shape of the object
+    /// in relation to its locaion
     pub shape: LocalPolygon,
+    /// The global position of center of the object
     pub location: Location,
+    /// The orientation of the object, measured in
+    /// radians within the range [0.0; 2π).
+    /// An orientation of 0.0 means that the
+    /// object is facing right.
     pub orientation: Radians,
+    /// The initial velocity of the object, defined
+    /// as a two dimensional vector relative to the
+    /// objects center
     pub velocity: Velocity,
+    /// The kind of object we are dealing with
     pub kind: Kind,
 }
 
@@ -37,25 +75,35 @@ pub struct GlobalVertex {
     pub y: u32,
 }
 
+/// A radian confined to the range of [0.0; 2π)
 #[derive(Debug, PartialEq, Copy, Clone, Default)]
 pub struct Radians(pub f32);
 
+/// A location within the world
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub struct Location {
     pub x: u32,
     pub y: u32,
 }
 
+/// The velocity of an object, measured as
+/// a two dimensional vector
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub struct Velocity {
     pub x: i32,
     pub y: i32,
 }
 
+/// The part of an object that is responsible for custom
+/// behavior and interactions
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub enum Kind {
+    /// An intelligent organism featuring a neural network
     Organism,
+    /// A self-spreading plant, ready for consumption
     Plant,
+    /// A stationary body of water
     Water,
+    /// Impassable terrain
     Terrain,
 }
