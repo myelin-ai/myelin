4de9ef076ee537eca30998426c019509de1ec7d6
Jan Nils Ferner <jan@myelin.ch>
2018-09-15 14:53:20 +0200

    Merge branch 'master' into worldgen-doc

--- visualization/src/bootstrapper.rs
+++ visualization/src/bootstrapper.rs
@@ -1,3 +1,6 @@
+//! Entrypoint for the crate,
+//! used to setup the entire visualization
+
 use crate::input_handler::InputHandler;
 use crate::presenter::CanvasPresenter;
 use crate::simulation::SimulationImpl;
@@ -8,6 +11,26 @@ use wasm_bindgen::prelude::*;
 
 use web_sys::HtmlCanvasElement;
 
+/// Initializes all components with explicit implementations
+/// and returns a [`InputHandler`] that one can use to signal
+/// user interaction. This function is intended to be called from
+/// JavaScript or, preferably, TypeScript.
+/// # Examples
+/// ```ts
+///    import('../out/myelin_visualization').then((wasm) => {
+///        const canvas = document.getElementById('visualization') as HTMLCanvasElement
+///        const inputHandler = wasm.init(canvas)
+///        inputHandler.on_timer()
+///    }).catch((reason) => {
+///        console.error(reason)
+///        document.body.appendChild(document.createTextNode('Failed to load WASM'))
+///        const reasonElement = document.createElement('pre')
+///        reasonElement.innerText = reason
+///        document.body.appendChild(reasonElement)
+///    })
+/// ```
+///
+/// [`InputHandler`]: ../input_handler/struct.InputHandler.html
 #[wasm_bindgen]
 pub fn init(canvas: &HtmlCanvasElement) -> InputHandler {
     let view = Box::new(CanvasView::new(canvas));
--- visualization/src/input_handler.rs
+++ visualization/src/input_handler.rs
@@ -1,6 +1,13 @@
+//! Functionality to communicate with the simulation
+//! once it's running.
+
 use crate::simulation::Simulation;
 use wasm_bindgen::prelude::*;
 
+/// Struct used to signal user interaction or events.
+/// Created by [`bootstrapper::init()`].
+///
+/// [`bootstrapper::init()`]: ../bootstrapper/fn.init.html
 #[wasm_bindgen]
 pub struct InputHandler {
     simulation: Box<dyn Simulation>,
@@ -12,6 +19,11 @@ impl InputHandler {
         Self { simulation }
     }
 
+    /// Signal the simulation that a timer event has been fired,
+    /// letting the simulation advance by one [`step`] and update
+    /// the view
+    ///
+    /// [`step`]: ../../myelin_environment/world/trait.World.html#tymethod.step
     pub fn on_timer(&mut self) {
         self.simulation.step();
     }
