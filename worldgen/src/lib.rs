6a80a7664bc1132753b534b6753b117e5d83f73a
Jan Nils Ferner <jan@myelin.ch>
2018-09-15 13:35:20 +0200

    Document lib.rs

--- worldgen/src/lib.rs
+++ worldgen/src/lib.rs
@@ -1,9 +1,21 @@
+//! Generate [`Worlds`] using pre defined world
+//! generation algorithms
+//!
+//! [`Worlds`]: ../myelin_environment/world/trait.World.html
+
 #![deny(rust_2018_idioms)]
 
 use myelin_environment::world::World;
 
 pub mod generator;
 
+/// API for [`World`] generation
+///
+/// [`World`]: ../myelin_environment/world/trait.World.html
 pub trait WorldGenerator {
+    /// Generate a new [`World`] and populate it with [`Objects`]
+    ///
+    /// [`World`]: ../myelin_environment/world/trait.World.html
+    /// [`Objects`]: ../myelin_environment/object/struct.LocalObject.html
     fn generate(&self) -> Box<dyn World>;
 }
