88532b45daba723333ecbd5db2491d50e9e29119
Ruben Schmidmeister <ruben@myelin.ch>
2018-09-15 11:09:00 +0200

    Add comment explaining why unsafe block is safe

--- visualization/src/view/js.rs
+++ visualization/src/view/js.rs
@@ -1,13 +1,15 @@
 use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
 
 pub(crate) fn get_2d_context(canvas: &HtmlCanvasElement) -> CanvasRenderingContext2d {
-    const CONTEXT_TYPE: &str = "2d";
+    const CONTEXT_ID: &str = "2d";
     const ERROR_MESSAGE: &str = "unable to get 2d context";
 
     let context = canvas
-        .get_context(CONTEXT_TYPE)
+        .get_context(CONTEXT_ID)
         .expect(ERROR_MESSAGE)
         .expect(ERROR_MESSAGE);
 
+    // This is safe because get_context() always returns `CanvasRenderingContext2d`
+    // when the context id '2d' is passed.
     unsafe { std::mem::transmute(context) }
 }
