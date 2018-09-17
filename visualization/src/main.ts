1890d1841558e24f7f3a615559cbfc4c7cb2eb15
Ruben Schmidmeister <ruben@myelin.ch>
2018-09-17 21:31:05 +0200

    Rename simulatedTimestamp -> simulatedTimestep

--- visualization/src/main.ts
+++ visualization/src/main.ts
@@ -2,10 +2,10 @@ import('../out/myelin_visualization').then((wasm) => {
     const canvas = document.getElementById('visualization') as HTMLCanvasElement
     const inputHandler = wasm.init(canvas)
     const MILLIS_IN_SECOND = 1000
-    const simulatedTimestamp = wasm.simulated_timestamp() * MILLIS_IN_SECOND
+    const simulatedTimestep = wasm.simulated_timestep() * MILLIS_IN_SECOND
     const onTimer = () => inputHandler.on_timer()
     onTimer()
-    setInterval(onTimer, simulatedTimestamp)
+    setInterval(onTimer, simulatedTimestep)
 }).catch((reason) => {
     console.error(reason)
     document.body.appendChild(document.createTextNode('Failed to load WASM'))
