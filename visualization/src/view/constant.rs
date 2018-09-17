5265b6ea0f315f6b0603bd113e1eff4194d4276c
Ruben Schmidmeister <ruben@myelin.ch>
2018-09-17 19:48:45 +0200

    Register interval that calls step()
    Resolves #64

--- visualization/src/view/constant.rs
+++ visualization/src/view/constant.rs
@@ -1,8 +1,15 @@
-pub const SIMULATED_TIMESTEP: f64 = 1.0 / 60.0;
+use wasm_bindgen::prelude::*;
+
+pub(crate) const SIMULATED_TIMESTEP: f64 = 1.0 / 60.0;
 
 pub(crate) mod color {
     pub(crate) const ORANGE: &str = "orange";
     pub(crate) const BLUE: &str = "blue";
     pub(crate) const GREEN: &str = "green";
     pub(crate) const BROWN: &str = "brown";
+}
+
+#[wasm_bindgen]
+pub fn simulated_timestamp() -> f64 {
+    SIMULATED_TIMESTEP
 }
