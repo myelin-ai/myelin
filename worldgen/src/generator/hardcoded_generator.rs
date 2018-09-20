892a74896a76eb3175b6d045cfbe6413c0f04d5d
Jan Nils Ferner <jan@myelin.ch>
2018-09-20 22:08:41 +0200

    Update worldgen to new API

--- worldgen/src/generator/hardcoded_generator.rs
+++ worldgen/src/generator/hardcoded_generator.rs
@@ -1,9 +1,9 @@
 //! A generator for a hardcoded simulation
 
 use crate::WorldGenerator;
-use myelin_environment::object::{Kind, Object, Radians};
-use myelin_environment::object_builder::{ObjectBuilder, PolygonBuilder};
-use myelin_environment::simulation::Simulation;
+use myelin_environment::object::{Kind, Location, Object, Position, Radians};
+use myelin_environment::object_builder::PolygonBuilder;
+use myelin_environment::simulation::{NewObject, Simulation};
 use std::f64::consts::FRAC_PI_2;
 
 /// Simulation generation algorithm that creates a fixed simulation
@@ -39,53 +39,42 @@ impl HardcodedGenerator {
             object_factory,
         }
     }
-}
 
-impl WorldGenerator for HardcodedGenerator {
-    fn generate(&self) -> Box<dyn Simulation> {
-        let mut simulation = (self.simulation_factory)();
-        populate_with_terrain(&mut *simulation);
-        populate_with_water(&mut *simulation);
-        populate_with_plants(&mut *simulation);
-        populate_with_organisms(&mut *simulation);
-        simulation
+    fn populate_with_terrain(&self, simulation: &mut dyn Simulation) {
+        simulation.add_object(self.build_terrain((25, 500), 50, 1000));
+        simulation.add_object(self.build_terrain((500, 25), 1000, 50));
+        simulation.add_object(self.build_terrain((975, 500), 50, 1000));
+        simulation.add_object(self.build_terrain((500, 975), 1000, 50));
     }
-}
 
-fn populate_with_terrain(simulation: &mut dyn Simulation) {
-    simulation.add_object_at(build_terrain((25, 500), 50, 1000));
-    simulation.add_object_at(build_terrain((500, 25), 1000, 50));
-    simulation.add_object_at(build_terrain((975, 500), 50, 1000));
-    simulation.add_object_at(build_terrain((500, 975), 1000, 50));
-}
+    fn build_terrain(&self, location: (u32, u32), width: i32, length: i32) -> NewObject {
+        // We add two pixels because of https://github.com/myelin-ai/myelin/issues/60
+        let x_offset = width / 2 + 2;
+        let y_offset = length / 2 + 2;
 
-fn build_terrain(location: (u32, u32), width: i32, length: i32) -> Object {
-    // We add two pixels because of https://github.com/myelin-ai/myelin/issues/60
-    let x_offset = width / 2 + 2;
-    let y_offset = length / 2 + 2;
-
-    let body = ObjectBuilder::new()
-        .shape(
-            PolygonBuilder::new()
+        NewObject {
+            object: (self.object_factory)(Kind::Terrain),
+            shape: PolygonBuilder::new()
                 .vertex(-x_offset, -y_offset)
                 .vertex(x_offset, -y_offset)
                 .vertex(x_offset, y_offset)
                 .vertex(-x_offset, y_offset)
                 .build()
                 .expect("Generated an invalid vertex"),
-        ).location(location.0, location.1)
-        .build()
-        .expect("Generated an invalid object");
-    LocalObject {
-        body,
-        behavior: unimplemented!(),
+            position: Position {
+                location: Location {
+                    x: location.0,
+                    y: location.1,
+                },
+                rotation: Radians::default(),
+            },
+        }
     }
-}
 
-fn populate_with_water(simulation: &mut dyn Simulation) {
-    let body = ObjectBuilder::new()
-        .shape(
-            PolygonBuilder::new()
+    fn populate_with_water(&self, simulation: &mut dyn Simulation) {
+        let object = NewObject {
+            object: (self.object_factory)(Kind::Water),
+            shape: PolygonBuilder::new()
                 .vertex(-180, 60)
                 .vertex(0, 200)
                 .vertex(180, 60)
@@ -93,94 +82,103 @@ fn populate_with_water(simulation: &mut dyn Simulation) {
                 .vertex(-100, -150)
                 .build()
                 .expect("Generated an invalid vertex"),
-        ).location(500, 500)
-        .build()
-        .expect("Generated an invalid object");
-    let object = LocalObject {
-        body,
-        behavior: unimplemented!(),
-    };
-    simulation.add_object(object);
-}
+            position: Position {
+                location: Location { x: 500, y: 500 },
+                rotation: Radians::default(),
+            },
+        };
+        simulation.add_object(object);
+    }
 
-fn populate_with_plants(simulation: &mut dyn Simulation) {
-    for i in 0..=10 {
-        for j in 0..=7 {
-            simulation.add_object(build_plant(100 + i * 30, 100 + j * 30));
+    fn populate_with_plants(&self, simulation: &mut dyn Simulation) {
+        for i in 0..=10 {
+            for j in 0..=7 {
+                simulation.add_object(self.build_plant(100 + i * 30, 100 + j * 30));
+            }
+        }
+        for i in 0..=10 {
+            for j in 0..=7 {
+                simulation.add_object(self.build_plant(600 + i * 30, 100 + j * 30));
+            }
         }
     }
-    for i in 0..=10 {
-        for j in 0..=7 {
-            simulation.add_object(build_plant(600 + i * 30, 100 + j * 30));
-        }
-    }
-}
 
-fn build_plant(x: u32, y: u32) -> LocalObject {
-    let body = ObjectBuilder::new()
-        .shape(
-            PolygonBuilder::new()
+    fn build_plant(&self, x: u32, y: u32) -> NewObject {
+        NewObject {
+            object: (self.object_factory)(Kind::Plant),
+            shape: PolygonBuilder::new()
                 .vertex(-10, -10)
                 .vertex(10, -10)
                 .vertex(10, 10)
                 .vertex(-10, 10)
                 .build()
                 .expect("Generated an invalid vertex"),
-        ).location(x, y)
-        .build()
-        .expect("Generated an invalid object");
-    LocalObject {
-        body,
-        behavior: unimplemented!(),
+            position: Position {
+                location: Location { x, y },
+                rotation: Radians::default(),
+            },
+        }
     }
-}
 
-fn populate_with_organisms(simulation: &mut dyn Simulation) {
-    simulation.add_object(build_organism(300, 800));
-    simulation.add_object(build_organism(400, 800));
-    simulation.add_object(build_organism(500, 800));
-    simulation.add_object(build_organism(600, 800));
-    simulation.add_object(build_organism(700, 800));
-}
+    fn populate_with_organisms(&self, simulation: &mut dyn Simulation) {
+        simulation.add_object(self.build_organism(300, 800));
+        simulation.add_object(self.build_organism(400, 800));
+        simulation.add_object(self.build_organism(500, 800));
+        simulation.add_object(self.build_organism(600, 800));
+        simulation.add_object(self.build_organism(700, 800));
+    }
 
-fn build_organism(x: u32, y: u32) -> LocalObject {
-    let body = ObjectBuilder::new()
-        .shape(
-            PolygonBuilder::new()
+    fn build_organism(&self, x: u32, y: u32) -> NewObject {
+        NewObject {
+            object: (self.object_factory)(Kind::Organism),
+            shape: PolygonBuilder::new()
                 .vertex(25, 0)
                 .vertex(-25, 20)
                 .vertex(-5, 0)
                 .vertex(-25, -20)
                 .build()
                 .expect("Generated an invalid vertex"),
-        ).location(x, y)
-        .orientation(Radians(FRAC_PI_2))
-        .build()
-        .expect("Generated an invalid object");
-    LocalObject {
-        body,
-        behavior: unimplemented!(),
+            position: Position {
+                location: Location { x, y },
+                rotation: Radians(FRAC_PI_2),
+            },
+        }
+    }
+}
+
+impl WorldGenerator for HardcodedGenerator {
+    fn generate(&self) -> Box<dyn Simulation> {
+        let mut simulation = (self.simulation_factory)();
+        self.populate_with_terrain(&mut *simulation);
+        self.populate_with_water(&mut *simulation);
+        self.populate_with_plants(&mut *simulation);
+        self.populate_with_organisms(&mut *simulation);
+        simulation
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use myelin_environment::object::{GlobalObject, LocalObject};
+    use myelin_environment::object::{
+        ImmovableAction, ImmovableObject, Kind, Location, Object, ObjectDescription, Position,
+        Radians,
+    };
+    use myelin_environment::simulation::NewObject;
 
     #[derive(Debug, Default)]
     struct SimulationMock {
-        objects: Vec<LocalObject>,
+        objects: Vec<NewObject>,
     }
 
     impl Simulation for SimulationMock {
         fn step(&mut self) {
             panic!("step() called unexpectedly")
         }
-        fn add_object(&mut self, object: LocalObject) {
+        fn add_object(&mut self, object: NewObject) {
             self.objects.push(object)
         }
-        fn objects(&self) -> Vec<GlobalObject> {
+        fn objects(&self) -> Vec<ObjectDescription> {
             panic!("objects() called unexpectedly")
         }
         fn set_simulated_timestep(&mut self, _: f64) {
@@ -193,10 +191,23 @@ mod tests {
         }
     }
 
+    #[derive(Debug)]
+    struct ObjectMock;
+    impl ImmovableObject for ObjectMock {
+        fn step(&mut self) -> Vec<ImmovableAction> {
+            panic!("step() was called unexpectedly")
+        }
+        fn kind(&self) -> Kind {
+            panic!("kind() was called unexpectedly")
+        }
+    }
+
     #[test]
     fn generates_simulation() {
-        let simulation_factory = || -> Box<dyn Simulation> { Box::new(SimulationMock::default()) };
-        let generator = HardcodedGenerator::new(Box::new(simulation_factory));
+        let simulation_factory =
+            Box::new(|| -> Box<dyn Simulation> { Box::new(SimulationMock::default()) });
+        let object_factory = Box::new(|_: Kind| Object::Immovable(Box::new(ObjectMock {})));
+        let generator = HardcodedGenerator::new(simulation_factory, object_factory);
 
         let _simulation = generator.generate();
     }
