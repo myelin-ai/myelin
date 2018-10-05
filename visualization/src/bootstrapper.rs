cec18c6eb3a11249958991e400cba94ec2a8195f
Ruben Schmidmeister <ruben@myelin.ch>
2018-10-05 14:56:50 +0200

    Empty input handler

--- visualization/src/bootstrapper.rs
+++ visualization/src/bootstrapper.rs
@@ -1,17 +1,7 @@
 //! Entrypoint for the crate,
 //! used to setup the entire visualization
 
-use crate::controller::ControllerImpl;
 use crate::input_handler::InputHandler;
-use crate::presenter::CanvasPresenter;
-use crate::view::constant::SIMULATED_TIMESTEP;
-use crate::view::CanvasView;
-use myelin_environment::object::{Kind, ObjectBehavior};
-use myelin_environment::simulation_impl::world::rotation_translator::NphysicsRotationTranslatorImpl;
-use myelin_environment::simulation_impl::world::NphysicsWorld;
-use myelin_environment::{simulation_impl::SimulationImpl, Simulation};
-use myelin_object_behavior::Static;
-use myelin_worldgen::generator::HardcodedGenerator;
 use wasm_bindgen::prelude::*;
 use web_sys::HtmlCanvasElement;
 
@@ -36,19 +26,6 @@ use web_sys::HtmlCanvasElement;
 ///
 /// [`InputHandler`]: ../input_handler/struct.InputHandler.html
 #[wasm_bindgen]
-pub fn init(canvas: &HtmlCanvasElement) -> InputHandler {
-    let view = Box::new(CanvasView::new(canvas));
-    let presenter = Box::new(CanvasPresenter::new(view));
-    let simulation_factory = Box::new(|| -> Box<dyn Simulation> {
-        let rotation_translator = NphysicsRotationTranslatorImpl::default();
-        let world = Box::new(NphysicsWorld::with_timestep(
-            SIMULATED_TIMESTEP,
-            Box::new(rotation_translator),
-        ));
-        Box::new(SimulationImpl::new(world))
-    });
-    let object_factory = Box::new(|_: Kind| -> Box<dyn ObjectBehavior> { Box::new(Static::new()) });
-    let worldgen = HardcodedGenerator::new(simulation_factory, object_factory);
-    let controller = Box::new(ControllerImpl::new(presenter, &worldgen));
-    InputHandler::new(controller)
+pub fn init(_canvas: &HtmlCanvasElement) -> InputHandler {
+    InputHandler::new()
 }
--- visualization/src/input_handler.rs
+++ visualization/src/input_handler.rs
@@ -1,7 +1,6 @@
 //! Functionality to communicate with the controller
 //! once it's running.
 
-use crate::controller::Controller;
 use wasm_bindgen::prelude::*;
 
 /// Struct used to signal user interaction or events.
@@ -11,22 +10,11 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 #[derive(Debug)]
-pub struct InputHandler {
-    controller: Box<dyn Controller>,
-}
+pub struct InputHandler;
 
 #[wasm_bindgen]
 impl InputHandler {
-    pub(crate) fn new(controller: Box<dyn Controller>) -> Self {
-        Self { controller }
-    }
-
-    /// Signal the controller that a timer event has been fired,
-    /// letting the controller advance by one [`step`] and update
-    /// the view
-    ///
-    /// [`step`]: ../../myelin_environment/world/trait.World.html#tymethod.step
-    pub fn on_timer(&mut self) {
-        self.controller.step();
+    pub(crate) fn new() -> Self {
+        InputHandler
     }
 }
