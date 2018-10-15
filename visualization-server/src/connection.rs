01e2c5a6c3979c6f7e656595f5d8cc7470b66192
Ruben Schmidmeister <ruben@myelin.ch>
2018-10-15 11:39:54 +0200

    Implement error type for WebsocketClient

--- visualization-server/src/connection.rs
+++ visualization-server/src/connection.rs
@@ -1,6 +1,8 @@
 use std::error::Error;
-use std::fmt::{self, Debug};
+use std::fmt::{self, Debug, Display};
+use std::io::ErrorKind as IoErrorKind;
 use websocket::client::sync::Client;
+use websocket::result::WebSocketError;
 use websocket::stream::sync::TcpStream;
 
 #[derive(Debug)]
@@ -23,7 +25,7 @@ impl PartialEq for Connection {
     }
 }
 
-struct WebsocketClient(Client<TcpStream>);
+pub(crate) struct WebsocketClient(Client<TcpStream>);
 
 impl Debug for WebsocketClient {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -34,5 +36,38 @@ impl Debug for WebsocketClient {
 impl Socket for WebsocketClient {
     fn send_message(&mut self, payload: &[u8]) -> Result<(), Box<dyn SocketError>> {
         unimplemented!();
+    }
+}
+
+#[derive(Debug)]
+struct WebsocketClientError(WebSocketError);
+
+impl From<WebSocketError> for WebsocketClientError {
+    fn from(error: WebSocketError) -> Self {
+        WebsocketClientError(error)
+    }
+}
+
+impl Error for WebsocketClientError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        Some(&self.0)
+    }
+}
+
+impl SocketError for WebsocketClientError {
+    fn is_broken_pipe(&self) -> bool {
+        if let WebSocketError::IoError(err) = self.0 {
+            if let IoErrorKind::BrokenPipe = err.kind() {
+                return true;
+            }
+        };
+
+        false
+    }
+}
+
+impl Display for WebsocketClientError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
     }
 }
