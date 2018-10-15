bc204814b77c0acac83001956bfdef0fae6b900b
Mathias Fischler <mathias@myelin.ch>
2018-10-15 10:28:03 +0200

    Move JsonSerializer struct def and tests to json.rs
    Also remove phantom date usage

--- visualization-core/src/serialization/json.rs
+++ visualization-core/src/serialization/json.rs
@@ -1,12 +1,13 @@
-use crate::serialization::JsonSerializer;
 use crate::serialization::ViewModelSerializer;
 use crate::view_model_delta::ViewModelDelta;
 use std::error::Error;
-use std::marker::PhantomData;
+
+#[derive(Debug)]
+pub struct JsonSerializer;
 
 impl JsonSerializer {
     pub fn new() -> Self {
-        JsonSerializer(PhantomData)
+        JsonSerializer
     }
 }
 
@@ -18,5 +19,76 @@ impl ViewModelSerializer for JsonSerializer {
         let serialized = serde_json::to_string(view_model_delta)?;
 
         Ok(serialized.into())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::view_model_delta::*;
+    use myelin_environment::object::*;
+    use myelin_environment::object_builder::PolygonBuilder;
+
+    #[test]
+    fn serializes_full_delta() {
+        let expected: Vec<u8> = r#"{"objects":[{"shape":{"vertices":[{"x":1,"y":1},{"x":2,"y":3},{"x":5,"y":6}]},"kind":"Organism"}]}"#.into();
+
+        let view_model_delta = ViewModelDelta {
+            objects: vec![ObjectDescriptionDelta {
+                id: 12,
+                kind: Some(Kind::Organism),
+                shape: Some(
+                    PolygonBuilder::new()
+                        .vertex(-5, -5)
+                        .vertex(1, 1)
+                        .vertex(2, 3)
+                        .vertex(5, 6)
+                        .build()
+                        .unwrap(),
+                ),
+                mobility: Some(Mobility::Movable(Velocity { x: 2, y: 3 })),
+                position: Some(Position {
+                    location: Location { x: 3, y: 4 },
+                    rotation: Radians::new(1.0).unwrap(),
+                }),
+                sensor: Some(Some(Sensor {
+                    shape: PolygonBuilder::new()
+                        .vertex(-10, -12)
+                        .vertex(10, 6)
+                        .vertex(16, 0)
+                        .build()
+                        .unwrap(),
+                    position: Position {
+                        location: Location { x: 2, y: 3 },
+                        rotation: Radians::new(-1.0).unwrap(),
+                    },
+                })),
+            }],
+            deleted_objects: Vec::new(),
+        };
+
+        let serializer = JsonSerializer::new();
+        let serialized = serializer
+            .serialize_view_model_delta(&view_model_delta)
+            .unwrap();
+
+        assert_eq!(expected, serialized);
+    }
+
+    #[test]
+    fn serialize_works_with_empty_view_model() {
+        let expected: Vec<u8> = r#"{"objects":[]}"#.into();
+
+        let view_model_delta = ViewModelDelta {
+            objects: Vec::new(),
+            deleted_objects: Vec::new(),
+        };
+
+        let serializer = JsonSerializer::new();
+        let serialized = serializer
+            .serialize_view_model_delta(&view_model_delta)
+            .unwrap();
+
+        assert_eq!(expected, serialized);
     }
 }
