d37cce4625cd72138e8fd85d2e10ef2562cef324
Ruben Schmidmeister <ruben@myelin.ch>
2018-10-17 23:02:11 +0200

    Add maplit crate as dev-dependency

--- visualization-core/src/lib.rs
+++ visualization-core/src/lib.rs
@@ -9,6 +9,10 @@
 #[macro_use]
 extern crate serde_derive;
 
+#[cfg_attr(test, macro_use)]
+#[cfg(test)]
+extern crate maplit;
+
 pub mod serialization;
 pub mod transmission;
 pub mod view_model_delta;
