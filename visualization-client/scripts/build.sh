0826d56472936e6c53efe0deacd3ca362bc3ee48
Ruben Schmidmeister <ruben@myelin.ch>
2018-10-19 15:14:02 +0200

    Pass directory safely to `cd`

--- visualization-client/scripts/build.sh
+++ visualization-client/scripts/build.sh
@@ -6,11 +6,11 @@ crate_dir=$(cd -- "$(dirname -- "$0")/.." && pwd)
 application_name=myelin_visualization_client
 target_dir="$crate_dir/out"
 
-(cd $crate_dir && cargo build --target wasm32-unknown-unknown)
+(cd -- "$crate_dir" && cargo build --target wasm32-unknown-unknown)
 
 rm -rf -- "$target_dir"
 mkdir -- "$target_dir"
 wasm-bindgen "$crate_dir/../target/wasm32-unknown-unknown/debug/$application_name.wasm" \
              --out-dir "$target_dir"
 
-(cd $crate_dir && yarn && yarn webpack)
+(cd -- "$crate_dir" && yarn && yarn webpack)
