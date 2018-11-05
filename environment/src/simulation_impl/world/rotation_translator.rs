d0cab87aec8660c5b0acad60d44073ed2f175043
Jeremy Stucki <jeremy@myelin.ch>
2018-11-05 08:28:28 +0100

    Fix typo

--- environment/src/simulation_impl/world/rotation_translator.rs
+++ environment/src/simulation_impl/world/rotation_translator.rs
@@ -121,12 +121,12 @@ mod tests {
 
     #[test]
     fn to_nphysics_rotation_returns_0_when_passed_0() {
-        verify_to_nphysics_rotation_returns_exoected_result(Radians::try_new(0.0).unwrap(), 0.0)
+        verify_to_nphysics_rotation_returns_expected_result(Radians::try_new(0.0).unwrap(), 0.0)
     }
 
     #[test]
     fn to_nphysics_rotation_returns_half_pi_when_passed_half_pi() {
-        verify_to_nphysics_rotation_returns_exoected_result(
+        verify_to_nphysics_rotation_returns_expected_result(
             Radians::try_new(FRAC_PI_2).unwrap(),
             FRAC_PI_2,
         )
@@ -134,12 +134,12 @@ mod tests {
 
     #[test]
     fn to_nphysics_rotation_returns_pi_when_passed_pi() {
-        verify_to_nphysics_rotation_returns_exoected_result(Radians::try_new(PI).unwrap(), PI)
+        verify_to_nphysics_rotation_returns_expected_result(Radians::try_new(PI).unwrap(), PI)
     }
 
     #[test]
     fn to_nphysics_rotation_returns_negative_half_pi_when_passed_one_and_a_half_pi() {
-        verify_to_nphysics_rotation_returns_exoected_result(
+        verify_to_nphysics_rotation_returns_expected_result(
             Radians::try_new(3.0 * FRAC_PI_2).unwrap(),
             -FRAC_PI_2,
         )
@@ -165,7 +165,7 @@ mod tests {
         verify_to_radians_returns_expected_result(-FRAC_PI_2, Radians::try_new(3.0 * FRAC_PI_2))
     }
 
-    fn verify_to_nphysics_rotation_returns_exoected_result(input: Radians, expected: f64) {
+    fn verify_to_nphysics_rotation_returns_expected_result(input: Radians, expected: f64) {
         let translator = NphysicsRotationTranslatorImpl::default();
         assert_eq!(expected, translator.to_nphysics_rotation(input));
     }
