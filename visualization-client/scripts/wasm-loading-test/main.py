fddd09348ad14011b453585f9a3434f5c88351ac
Ruben Schmidmeister <ruben@myelin.ch>
2018-11-09 19:47:03 +0100

    Catch fatal errors in browser console

--- visualization-client/scripts/wasm-loading-test/main.py
+++ visualization-client/scripts/wasm-loading-test/main.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 import os
 import subprocess
 import signal
+import time
+import sys
 
 HTTP_PORT = 8081
 
@@ -28,12 +30,33 @@ def _start_webdriver():
     options = webdriver.chrome.options.Options()
     options.headless = True
 
+    # Sleep for a bit to make sure server is started
+    time.sleep(2)
+
     with webdriver.Chrome(options=options) as driver:
         driver.get('http://localhost:{}'.format(HTTP_PORT))
+
+        # Sleep for a bit to make sure everything is properly loaded
+        time.sleep(2)
 
         body = driver.find_element_by_css_selector('body')
 
         assert 'Failed to initialize visualization' not in body.text
+
+        # Sleep for a bit, so panics that might be caused
+        # by a websocket message are caught
+        time.sleep(5)
+
+        severe_messages = [msg for msg in driver.get_log(
+            'browser') if msg['level'] == 'SEVERE']
+
+        if not len(severe_messages) == 0:
+            print('Error: fatal messages found in console')
+            for message in severe_messages:
+                print('')
+                print('source: {}'.format(message['source']))
+                print('message: {}'.format(message['message']))
+            sys.exit(1)
 
 
 httpd = _start_http_server()
