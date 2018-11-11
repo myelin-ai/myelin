d49a40e4c85dd3c280397264841e7cf1adb09525
Jan Nils Ferner <jan@myelin.ch>
2018-11-11 22:01:01 +0100

    Split global conversion into translation and rotation

--- geometry/src/polygon/mod.rs
+++ geometry/src/polygon/mod.rs
@@ -12,29 +12,44 @@ pub struct Polygon {
 }
 
 impl Polygon {
-    fn to_global_polygon(&self, position: &Position) -> Self {
-        let geo_polygon = GeoPolygon::new(
-            self.vertices
-                .iter()
-                .map(|vertex| (vertex.x, vertex.y))
-                .collect::<Vec<_>>()
-                .into(),
-            vec![],
-        );
-        let center = GeoPoint::new(position.location.x, position.location.y);
-        let geo_polygon = geo_polygon.translate(center.x(), center.y());
-        let rotation_angle_in_degrees = position.rotation.value().to_degrees();
-        let geo_polygon = geo_polygon.rotate_around_point(rotation_angle_in_degrees, center);
-        let vertices = geo_polygon
-            .exterior
-            .into_points()
+    /// Apply translation specified by `translation`, represented as
+    /// a relative point
+    pub fn translate(&self, translation: Point) -> Self {
+        let translated_vertices = self
+            .vertices
             .iter()
-            .map(|point| Point {
-                x: point.x(),
-                y: point.y(),
+            .map(|vertex| Point {
+                x: vertex.x + translation.x,
+                y: vertex.y + translation.y,
             })
             .collect();
-        Polygon { vertices }
+
+        Polygon {
+            vertices: translated_vertices,
+        }
+    }
+
+    /// Rotate polygon by a `rotation` around a `point`
+    pub fn rotate_around_point(&self, rotation: Radians, point: Point) -> Self {
+        let rotation = rotation.value();
+        let rotated_vertices = self
+            .vertices
+            .iter()
+            .map(|vertex| {
+                let delta_x = vertex.x - point.x;
+                let delta_y = vertex.y - point.y;
+                let (rotation_sin, rotation_cos) = rotation.sin_cos();
+                let rotated_x = rotation_cos * delta_x + rotation_sin * delta_y + point.x;
+                let rotated_y = -rotation_sin * delta_x + rotation_cos * delta_y + point.x;
+                Point {
+                    x: rotated_x,
+                    y: rotated_y,
+                }
+            })
+            .collect();
+        Self {
+            vertices: rotated_vertices,
+        }
     }
 }
 
@@ -44,25 +59,26 @@ mod test {
     use super::*;
     use std::f64::consts::PI;
 
-    fn polygon() -> Polygon {
-        PolygonBuilder::default()
-            .vertex(-10.0, -10.0)
-            .vertex(10.0, -10.0)
-            .vertex(10.0, 10.0)
-            .vertex(-10.0, 10.0)
-            .build()
-            .unwrap()
+    fn polygon_and_center() -> (Polygon, Point) {
+        (
+            PolygonBuilder::default()
+                .vertex(-10.0, -10.0)
+                .vertex(10.0, -10.0)
+                .vertex(10.0, 10.0)
+                .vertex(-10.0, 10.0)
+                .build()
+                .unwrap(),
+            Point { x: 0.0, y: 0.0 },
+        )
     }
 
-    fn position(rotation: Radians) -> Position {
-        Position {
-            location: Point { x: 30.0, y: 40.0 },
-            rotation,
-        }
+    fn translation() -> Point {
+        Point { x: 30.0, y: 40.0 }
     }
 
     #[test]
-    fn converts_to_global_object_with_no_orientation() {
+    fn translates() {
+        let (polygon, _) = polygon_and_center();
         assert_eq!(
             Polygon {
                 vertices: vec![
@@ -72,49 +88,100 @@ mod test {
                     Point { x: 20.0, y: 50.0 },
                 ],
             },
-            polygon().to_global_polygon(&position(Radians::default()))
+            polygon.translate(translation())
         );
     }
 
     #[test]
-    fn converts_to_global_object_with_pi_orientation() {
-        assert_eq!(
-            Polygon {
-                vertices: vec![
-                    Point { x: 40.0, y: 50.0 },
-                    Point { x: 20.0, y: 50.0 },
-                    Point { x: 20.0, y: 30.0 },
-                    Point { x: 40.0, y: 30.0 },
-                ],
-            },
-            polygon().to_global_polygon(&position(Radians::try_new(PI).unwrap()))
-        );
-    }
+    fn rotates_by_pi() {
+        let (polygon, center) = polygon_and_center();
 
-    #[test]
-    fn converts_to_global_object_with_arbitrary_orientation() {
+        const FLOATING_POINT_INACCURACY: f64 = 0.000000000000002;
         assert_eq!(
             Polygon {
                 vertices: vec![
                     Point {
-                        x: 41.311125046603124,
-                        y: 48.48872488540578
+                        x: 10.0 - FLOATING_POINT_INACCURACY,
+                        y: 10.0 + FLOATING_POINT_INACCURACY
                     },
                     Point {
-                        x: 21.51127511459422,
-                        y: 51.311125046603124
+                        x: -10.0 - FLOATING_POINT_INACCURACY,
+                        y: 10.0 - FLOATING_POINT_INACCURACY
+                    },
+                    Point {
+                        x: -10.0 + FLOATING_POINT_INACCURACY,
+                        y: -10.0 - FLOATING_POINT_INACCURACY
+                    },
+                    Point {
+                        x: 10.0 + FLOATING_POINT_INACCURACY,
+                        y: -10.0 + FLOATING_POINT_INACCURACY
+                    },
+                ],
+            },
+            polygon.rotate_around_point(Radians::try_new(PI).unwrap(), center)
+        );
+    }
+
+    #[test]
+    fn rotates_by_arbitrary_orientation() {
+        let (polygon, center) = polygon_and_center();
+
+        const ROTATION_A: f64 = 8.488724885405782;
+        const ROTATION_B: f64 = 11.311125046603125;
+
+        assert_eq!(
+            Polygon {
+                vertices: vec![
+                    Point {
+                        x: ROTATION_A,
+                        y: ROTATION_B
+                    },
+                    Point {
+                        x: -ROTATION_B,
+                        y: ROTATION_A
+                    },
+                    Point {
+                        x: -ROTATION_A,
+                        y: -ROTATION_B
+                    },
+                    Point {
+                        x: ROTATION_B,
+                        y: -ROTATION_A
+                    },
+                ],
+            },
+            polygon.rotate_around_point(Radians::try_new(3.0).unwrap(), center)
+        );
+    }
+
+    #[test]
+    fn translates_and_rotates() {
+        let (polygon, _) = polygon_and_center();
+        let translation = translation();
+        let translated_polygon = polygon.translate(translation);
+
+        assert_eq!(
+            Polygon {
+                vertices: vec![
+                    Point {
+                        x: 38.48872488540578,
+                        y: 41.311125046603124
                     },
                     Point {
                         x: 18.688874953396876,
-                        y: 31.51127511459422
+                        y: 38.48872488540578
                     },
                     Point {
-                        x: 38.48872488540578,
-                        y: 28.688874953396876
+                        x: 21.51127511459422,
+                        y: 18.688874953396876
+                    },
+                    Point {
+                        x: 41.311125046603124,
+                        y: 21.51127511459422
                     },
                 ],
             },
-            polygon().to_global_polygon(&position(Radians::try_new(3.0).unwrap()))
+            translated_polygon.rotate_around_point(Radians::try_new(3.0).unwrap(), translation)
         );
     }
 }
