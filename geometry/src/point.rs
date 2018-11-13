bc6327e176ed1a2626aae9503b19f5b9b97bec51
Jan Nils Ferner <jan@myelin.ch>
2018-11-13 11:04:41 +0100

    Improve tests

--- geometry/src/point.rs
+++ geometry/src/point.rs
@@ -57,72 +57,90 @@ mod tests {
     #[test]
     fn adds_zero_point() {
         let original_point = Point { x: 12.0, y: 43.0 };
-        let additional_point = Point { x: 0.0, y: 0.0 };
+        let point_to_add = Point { x: 0.0, y: 0.0 };
         let expected_point = original_point;
-        let added_point = original_point + additional_point;
+        let added_point = original_point + point_to_add;
         assert_eq!(expected_point, added_point);
     }
 
     #[test]
     fn adds_other_point() {
         let original_point = Point { x: 12.0, y: 43.0 };
-        let additional_point = Point { x: 3.0, y: 1.0 };
+        let point_to_add = Point { x: 3.0, y: 1.0 };
         let expected_point = Point { x: 15.0, y: 44.0 };
-        let added_point = original_point + additional_point;
+        let added_point = original_point + point_to_add;
         assert_eq!(expected_point, added_point);
     }
 
     #[test]
     fn adds_negative_point() {
         let original_point = Point { x: 12.0, y: 43.0 };
-        let additional_point = Point { x: -10.0, y: -20.0 };
+        let point_to_add = Point { x: -10.0, y: -20.0 };
         let expected_point = Point { x: 2.0, y: 23.0 };
-        let added_point = original_point + additional_point;
+        let added_point = original_point + point_to_add;
         assert_eq!(expected_point, added_point);
     }
 
     #[test]
     fn adds_to_zero_point() {
         let original_point = Point { x: 12.0, y: 43.0 };
-        let additional_point = Point { x: -12.0, y: -43.0 };
+        let point_to_add = Point { x: -12.0, y: -43.0 };
         let expected_point = Point { x: 0.0, y: 0.0 };
-        let added_point = original_point + additional_point;
+        let added_point = original_point + point_to_add;
+        assert_eq!(expected_point, added_point);
+    }
+
+    #[test]
+    fn adds_when_negative() {
+        let original_point = Point { x: -12.0, y: -43.0 };
+        let point_to_add = Point { x: -4.0, y: -2.0 };
+        let expected_point = Point { x: -16.0, y: -45.0 };
+        let added_point = original_point + point_to_add;
         assert_eq!(expected_point, added_point);
     }
 
     #[test]
     fn subtracts_zero_point() {
         let original_point = Point { x: 12.0, y: 43.0 };
-        let subtracted_point = Point { x: 0.0, y: 0.0 };
+        let point_to_subtract = Point { x: 0.0, y: 0.0 };
         let expected_point = original_point;
-        let added_point = original_point - subtracted_point;
-        assert_eq!(expected_point, added_point);
+        let substracted_point = original_point - point_to_subtract;
+        assert_eq!(expected_point, substracted_point);
     }
 
     #[test]
     fn subtracts_other_point() {
         let original_point = Point { x: 12.0, y: 43.0 };
-        let subtracted_point = Point { x: 3.0, y: 1.0 };
+        let point_to_subtract = Point { x: 3.0, y: 1.0 };
         let expected_point = Point { x: 9.0, y: 42.0 };
-        let added_point = original_point - subtracted_point;
-        assert_eq!(expected_point, added_point);
+        let substracted_point = original_point - point_to_subtract;
+        assert_eq!(expected_point, substracted_point);
     }
 
     #[test]
     fn subtracts_negative_point() {
         let original_point = Point { x: 12.0, y: 43.0 };
-        let subtracted_point = Point { x: -10.0, y: -20.0 };
+        let point_to_subtract = Point { x: -10.0, y: -20.0 };
         let expected_point = Point { x: 22.0, y: 63.0 };
-        let added_point = original_point - subtracted_point;
-        assert_eq!(expected_point, added_point);
+        let substracted_point = original_point - point_to_subtract;
+        assert_eq!(expected_point, substracted_point);
     }
 
     #[test]
     fn subtracts_to_zero_point() {
         let original_point = Point { x: 12.0, y: 43.0 };
-        let subtracted_point = original_point;
+        let point_to_subtract = original_point;
         let expected_point = Point { x: 0.0, y: 0.0 };
-        let added_point = original_point - subtracted_point;
-        assert_eq!(expected_point, added_point);
+        let point_to_subtract = original_point - point_to_subtract;
+        assert_eq!(expected_point, point_to_subtract);
+    }
+
+    #[test]
+    fn subtracts_when_negative() {
+        let original_point = Point { x: -12.0, y: -43.0 };
+        let point_to_subtract = Point { x: -4.0, y: -2.0 };
+        let expected_point = Point { x: -8.0, y: -41.0 };
+        let substracted_point = original_point - point_to_subtract;
+        assert_eq!(expected_point, substracted_point);
     }
 }
--- geometry/src/vector.rs
+++ geometry/src/vector.rs
@@ -57,72 +57,90 @@ mod tests {
     #[test]
     fn adds_zero_vector() {
         let original_vector = Vector { x: 12.0, y: 43.0 };
-        let additional_vector = Vector { x: 0.0, y: 0.0 };
+        let vector_to_add = Vector { x: 0.0, y: 0.0 };
         let expected_vector = original_vector;
-        let added_vector = original_vector + additional_vector;
+        let added_vector = original_vector + vector_to_add;
         assert_eq!(expected_vector, added_vector);
     }
 
     #[test]
     fn adds_other_vector() {
         let original_vector = Vector { x: 12.0, y: 43.0 };
-        let additional_vector = Vector { x: 3.0, y: 1.0 };
+        let vector_to_add = Vector { x: 3.0, y: 1.0 };
         let expected_vector = Vector { x: 15.0, y: 44.0 };
-        let added_vector = original_vector + additional_vector;
+        let added_vector = original_vector + vector_to_add;
         assert_eq!(expected_vector, added_vector);
     }
 
     #[test]
     fn adds_negative_vector() {
         let original_vector = Vector { x: 12.0, y: 43.0 };
-        let additional_vector = Vector { x: -10.0, y: -20.0 };
+        let vector_to_add = Vector { x: -10.0, y: -20.0 };
         let expected_vector = Vector { x: 2.0, y: 23.0 };
-        let added_vector = original_vector + additional_vector;
+        let added_vector = original_vector + vector_to_add;
         assert_eq!(expected_vector, added_vector);
     }
 
     #[test]
     fn adds_to_zero_vector() {
         let original_vector = Vector { x: 12.0, y: 43.0 };
-        let additional_vector = Vector { x: -12.0, y: -43.0 };
+        let vector_to_add = Vector { x: -12.0, y: -43.0 };
         let expected_vector = Vector { x: 0.0, y: 0.0 };
-        let added_vector = original_vector + additional_vector;
+        let added_vector = original_vector + vector_to_add;
+        assert_eq!(expected_vector, added_vector);
+    }
+
+    #[test]
+    fn adds_when_negative() {
+        let original_vector = Vector { x: -12.0, y: -43.0 };
+        let vector_to_add = Vector { x: -4.0, y: -2.0 };
+        let expected_vector = Vector { x: -16.0, y: -45.0 };
+        let added_vector = original_vector + vector_to_add;
         assert_eq!(expected_vector, added_vector);
     }
 
     #[test]
     fn subtracts_zero_vector() {
         let original_vector = Vector { x: 12.0, y: 43.0 };
-        let subtracted_vector = Vector { x: 0.0, y: 0.0 };
+        let vector_to_subtract = Vector { x: 0.0, y: 0.0 };
         let expected_vector = original_vector;
-        let added_vector = original_vector - subtracted_vector;
-        assert_eq!(expected_vector, added_vector);
+        let substracted_vector = original_vector - vector_to_subtract;
+        assert_eq!(expected_vector, substracted_vector);
     }
 
     #[test]
     fn subtracts_other_vector() {
         let original_vector = Vector { x: 12.0, y: 43.0 };
-        let subtracted_vector = Vector { x: 3.0, y: 1.0 };
+        let vector_to_subtract = Vector { x: 3.0, y: 1.0 };
         let expected_vector = Vector { x: 9.0, y: 42.0 };
-        let added_vector = original_vector - subtracted_vector;
-        assert_eq!(expected_vector, added_vector);
+        let substracted_vector = original_vector - vector_to_subtract;
+        assert_eq!(expected_vector, substracted_vector);
     }
 
     #[test]
     fn subtracts_negative_vector() {
         let original_vector = Vector { x: 12.0, y: 43.0 };
-        let subtracted_vector = Vector { x: -10.0, y: -20.0 };
+        let vector_to_subtract = Vector { x: -10.0, y: -20.0 };
         let expected_vector = Vector { x: 22.0, y: 63.0 };
-        let added_vector = original_vector - subtracted_vector;
-        assert_eq!(expected_vector, added_vector);
+        let substracted_vector = original_vector - vector_to_subtract;
+        assert_eq!(expected_vector, substracted_vector);
     }
 
     #[test]
     fn subtracts_to_zero_vector() {
         let original_vector = Vector { x: 12.0, y: 43.0 };
-        let subtracted_vector = original_vector;
+        let vector_to_subtract = original_vector;
         let expected_vector = Vector { x: 0.0, y: 0.0 };
-        let added_vector = original_vector - subtracted_vector;
-        assert_eq!(expected_vector, added_vector);
+        let vector_to_subtract = original_vector - vector_to_subtract;
+        assert_eq!(expected_vector, vector_to_subtract);
+    }
+
+    #[test]
+    fn subtracts_when_negative() {
+        let original_vector = Vector { x: -12.0, y: -43.0 };
+        let vector_to_subtract = Vector { x: -4.0, y: -2.0 };
+        let expected_vector = Vector { x: -8.0, y: -41.0 };
+        let substracted_vector = original_vector - vector_to_subtract;
+        assert_eq!(expected_vector, substracted_vector);
     }
 }
