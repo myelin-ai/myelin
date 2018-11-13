f91593188900bdff0c391fae5f641854a20d1817
Jan Nils Ferner <jan@myelin.ch>
2018-11-13 10:46:12 +0100

    Implement Add and Sub for Vector and Point

--- geometry/src/lib.rs
+++ geometry/src/lib.rs
@@ -17,20 +17,8 @@ pub use self::radians::*;
 mod polygon;
 pub use self::polygon::*;
 
-/// A vector
-#[derive(Debug, PartialEq, Copy, Clone, Default, Serialize, Deserialize)]
-pub struct Vector {
-    /// The x component of the Vector
-    pub x: f64,
-    /// The y component of the Vector
-    pub y: f64,
-}
+mod vector;
+pub use self::vector::*;
 
-/// A point in space
-#[derive(Debug, PartialEq, Copy, Clone, Default, Serialize, Deserialize)]
-pub struct Point {
-    /// The x coordinate of the Point
-    pub x: f64,
-    /// The y coordinate of the Point
-    pub y: f64,
-}
+mod point;
+pub use self::point::*;
