0e5abb7c19e5e152f46704027e3838be3be167f3
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-11-14 10:11:06 +0100

    Add docs

--- geometry/src/polygon/mod.rs
+++ geometry/src/polygon/mod.rs
@@ -51,43 +51,58 @@ impl Polygon {
 
     /// Checks if a given point rests inside the polygon
     pub fn contains_point(&self, point: Point) -> bool {
+        // Anything less than a line cannot "contain" anything
         if self.vertices.len() < 2 {
             return false;
         }
 
+        /// The side that a [`Point`] lies on, from the
+        /// point of view of a line
         #[derive(Eq, PartialEq, Debug)]
         enum Side {
+            /// The point lies to the right of the line
             Right,
+            /// The point lies to the left of the line
             Left,
-            None,
+            /// The point is exactly on the line
+            OnTheLine,
         };
 
+        /// Calculate which on which side of a line from `a` to `b` a
+        /// given `point` is
         fn calculate_facing_side(a: Vector, b: Vector, point: Vector) -> Side {
             let side_vector = b - a;
             let vector_from_a_to_point = point - a;
             let cross_product = side_vector.cross_product(vector_from_a_to_point);
 
+            /// Minimal distance from `point` to the line to be considered
+            /// exactly *on* the line
             const EPSILON: f64 = 0.000001;
             if cross_product < -EPSILON {
                 Side::Left
             } else if cross_product > EPSILON {
                 Side::Right
             } else {
-                Side::None
+                Side::OnTheLine
             }
         };
 
         let vector_to_point: Vector = point.into();
+        // The following unwraps are safe, as we do an
+        // early return if we don't contain at least 2 vertices
         let vector_to_last_point: Vector = (*self.vertices.last().unwrap()).into();
         let vector_to_first_point: Vector = (*self.vertices.first().unwrap()).into();
         let reference_side =
             calculate_facing_side(vector_to_last_point, vector_to_first_point, vector_to_point);
 
+        // If the point lies on the same side of all lines of the polygon,
+        // the point is contained in the polygon.
+        // We iterate until len() - 1 because inside the loop, we access i + 1.
         for i in 0..self.vertices.len() - 1 {
             let vector_to_point_a: Vector = self.vertices[i].into();
             let vector_to_point_b: Vector = self.vertices[i + 1].into();
             let side = calculate_facing_side(vector_to_point_a, vector_to_point_b, vector_to_point);
-            if side != reference_side && side != Side::None {
+            if side != reference_side && side != Side::OnTheLine {
                 return false;
             }
         }
