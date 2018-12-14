136d6e18375023a9e25308a25810e18d5a724dd1
Ruben Schmidmeister <ruben@myelin.ch>
2018-12-14 14:58:01 +0100

    Remove PartialOrd derives

--- visualization-client/src/view_model.rs
+++ visualization-client/src/view_model.rs
@@ -1,21 +1,21 @@
-#[derive(Debug, PartialEq, Clone, PartialOrd)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Object {
     pub shape: Polygon,
     pub kind: Kind,
 }
 
-#[derive(Debug, PartialEq, Clone, PartialOrd)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Polygon {
     pub vertices: Vec<Point>,
 }
 
-#[derive(Debug, PartialEq, Clone, PartialOrd)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Point {
     pub x: f64,
     pub y: f64,
 }
 
-#[derive(Debug, Eq, PartialEq, Clone, Ord, PartialOrd)]
+#[derive(Debug, Eq, PartialEq, Clone, Ord)]
 pub enum Kind {
     Organism,
     Plant,
