dfd968b74df6478e1ab96410c917aa3f709590ae
Jeremy Stucki <jeremy@myelin.ch>
2018-12-17 14:21:26 +0100

    Update tests

--- visualization-core/src/serialization/json.rs
+++ visualization-core/src/serialization/json.rs
@@ -58,13 +58,14 @@ mod test {
     use myelin_environment::object::*;
     use myelin_geometry::*;
 
-    const EXPECTED_JSON: &str = r#"{"12":{"Updated":{"shape":{"vertices":[{"x":-5.0,"y":-5.0},{"x":1.0,"y":1.0},{"x":2.0,"y":3.0},{"x":5.0,"y":6.0}]},"location":{"x":3.0,"y":4.0},"rotation":{"value":1.0},"mobility":{"Movable":{"x":2.0,"y":3.0}},"kind":"Organism"}}}"#;
+    const EXPECTED_JSON: &str = r#"{"12":{"Updated":{"name":"Cat","shape":{"vertices":[{"x":-5.0,"y":-5.0},{"x":1.0,"y":1.0},{"x":2.0,"y":3.0},{"x":5.0,"y":6.0}]},"location":{"x":3.0,"y":4.0},"rotation":{"value":1.0},"mobility":{"Movable":{"x":2.0,"y":3.0}},"kind":"Organism"}}}"#;
 
     #[test]
     fn serializes_full_delta() {
         let expected: Vec<u8> = EXPECTED_JSON.into();
 
         let object_description_delta = ObjectDescriptionDelta {
+            name: Some(Some(String::from("Cat"))),
             kind: Some(Kind::Organism),
             shape: Some(
                 PolygonBuilder::default()
@@ -118,6 +119,7 @@ mod test {
     #[test]
     fn deserializes_full_viewmodel() {
         let object_description_delta = ObjectDescriptionDelta {
+            name: Some(Some(String::from("Cat"))),
             kind: Some(Kind::Organism),
             shape: Some(
                 PolygonBuilder::default()
