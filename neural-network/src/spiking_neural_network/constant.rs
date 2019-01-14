a39134eca41ba784ad3643774446165d2be57f1d
Jan Nils Ferner <jan@myelin.ch>
2019-01-14 15:04:55 +0100

    Add some duration constants

--- neural-network/src/spiking_neural_network/constant.rs
+++ neural-network/src/spiking_neural_network/constant.rs
@@ -1,8 +1,6 @@
 //! [Source](https://books.google.com/books?id=A8H_9S4E0I4C&pg=PA55&lpg=PA55&focus=viewport)
 
-use crate::MembranePotential;
-#[cfg(test)]
-use crate::Milliseconds;
+use crate::{MembranePotential, Milliseconds};
 
 pub(crate) const RESTING_POTENTIAL: MembranePotential = MembranePotential(-70.0);
 pub(crate) const THRESHOLD_POTENTIAL: MembranePotential = MembranePotential(-55.0);
@@ -10,5 +8,12 @@ pub(crate) const ACTION_POTENTIAL: MembranePotential = MembranePotential(40.0);
 
 pub(crate) const PASSIVE_REPOLARIZATION_FACTOR: f64 = 0.01;
 
+pub(crate) const DEPOLARIZATION_DURATION: Milliseconds = Milliseconds(0.7);
+pub(crate) const ACTION_POTENTIAL_DURATION: Milliseconds = Milliseconds(0.1);
+pub(crate) const REPOLARIZATION_DURATION: Milliseconds = Milliseconds(0.7);
+pub(crate) const REFRACTORY_PERIOD_DURATION: Milliseconds = Milliseconds(3.5);
+
 #[cfg(test)]
-pub(crate) const SPIKE_DURATION: Milliseconds = Milliseconds(1.5);
+pub(crate) const SPIKE_DURATION: Milliseconds = Milliseconds(
+    DEPOLARIZATION_DURATION.0 + ACTION_POTENTIAL_DURATION.0 + REPOLARIZATION_DURATION.0,
+);
