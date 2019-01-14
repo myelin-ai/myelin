b87d047baa4254c7c28a868f8c9cdedf0354f84b
Jan Nils Ferner <jan@myelin.ch>
2019-01-14 11:41:10 +0100

    Improve neural plotting

--- neural-network/examples/plot_neuron.rs
+++ neural-network/examples/plot_neuron.rs
@@ -5,8 +5,13 @@ use myelin_neural_network::spiking_neural_network::SpikingNeuralNetwork;
 use myelin_neural_network::{
     Connection, Handle, MembranePotential, Milliseconds, NeuralNetwork, Weight,
 };
+use std::collections::HashMap;
 
 fn main() {
+    const TIME_TO_SIMULATE: Milliseconds = Milliseconds(10.0);
+    const TIMESTEP: Milliseconds = Milliseconds(0.001);
+    const CONSTANT_INPUT: bool = true;
+
     let mut neural_network = SpikingNeuralNetwork::default();
     let sensor_handle = neural_network.push_neuron();
     let neuron_handle = neural_network.push_neuron();
@@ -15,15 +20,22 @@ fn main() {
         to: Handle(neuron_handle.0),
         weight: Weight(1.0),
     };
-    neural_network.add_connection(connection).unwrap();
 
-    let points: Vec<_> = (0..100)
+    neural_network.add_connection(connection).unwrap();
+    let threshold_inputs = hashmap! {
+        sensor_handle => MembranePotential(-55.0)
+    };
+    neural_network.step(TIMESTEP, &threshold_inputs);
+
+    let steps = f64::ceil(TIME_TO_SIMULATE.0 / TIMESTEP.0) as u32;
+    let points: Vec<_> = (0..steps)
         .map(|i| {
-            let elapsed_time = Milliseconds(1.0);
-            let inputs = hashmap! {
-                sensor_handle => MembranePotential(-10.0)
+            let inputs = if CONSTANT_INPUT {
+                threshold_inputs.clone()
+            } else {
+                HashMap::new()
             };
-            neural_network.step(elapsed_time, &inputs);
+            neural_network.step(TIMESTEP, &inputs);
 
             let sensor_membrane_potential = neural_network
                 .membrane_potential_of_neuron(sensor_handle)
@@ -32,21 +44,33 @@ fn main() {
                 .membrane_potential_of_neuron(neuron_handle)
                 .unwrap();
 
-            (i, sensor_membrane_potential, neuron_membrane_potential)
+            (
+                f64::from(i) * TIMESTEP.0,
+                sensor_membrane_potential,
+                neuron_membrane_potential,
+            )
         })
         .collect();
 
     let sensor_points: Vec<_> = points
         .iter()
         .cloned()
-        .filter_map(|(i, potential, _)| potential.map(|potential| (f64::from(i), potential.0)))
+        .map(|(i, potential, _)| {
+            potential
+                .map(|potential| (i, potential.0))
+                .unwrap_or((i, -55.0))
+        })
         .map(|(x, y)| format!("{},{}", x, y))
         .collect();
 
     let neuron_points: Vec<_> = points
         .iter()
         .cloned()
-        .filter_map(|(i, _, potential)| potential.map(|potential| (f64::from(i), potential.0)))
+        .map(|(i, _, potential)| {
+            potential
+                .map(|potential| (i, potential.0))
+                .unwrap_or((i, -55.0))
+        })
         .map(|(x, y)| format!("{},{}", x, y))
         .collect();
 
