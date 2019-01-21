674ffe1f7fe89d7a2b99cca902523d24576bad25
Jan Nils Ferner <jan@myelin.ch>
2019-01-21 17:36:58 +0100

    Deny intra_doc_link_resolution_failure

--- geometry/src/lib.rs
+++ geometry/src/lib.rs
@@ -4,8 +4,9 @@
     rust_2018_idioms,
     missing_debug_implementations,
     missing_docs,
+    intra_doc_link_resolution_failure,
     clippy::doc_markdown,
-    clippy::unimplemented
+    clippy::unimplemented,
 )]
 
 #[cfg_attr(test, macro_use)]
