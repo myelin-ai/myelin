6b316e1ab920ba9b3e419cd93831477b061a0314
Jan Nils Ferner <jan@myelin.ch>
2019-02-15 13:21:32 +0100

    Implement Clone for NeuralNetworkDeveloper

--- genetics/src/lib.rs
+++ genetics/src/lib.rs
@@ -1,5 +1,6 @@
 //! Genes, genomes and the mechanisms needed to evolve neural networks from them
 
+#![feature(specialization)]
 #![deny(
     rust_2018_idioms,
     missing_debug_implementations,
@@ -9,6 +10,7 @@
 )]
 
 use myelin_neural_network::NeuralNetwork;
+use std::fmt::Debug;
 
 /// The set of all genes in an organism
 #[derive(Debug)]
@@ -18,10 +20,36 @@ pub struct Genome;
 ///
 /// [`NeuralNetwork`]: ../myelin-neural-network/trait.NeuralNetwork.html
 /// [`Genome`]: ./struct.Genome.html
-pub trait NeuralNetworkDeveloper {
+pub trait NeuralNetworkDeveloper: Debug + NeuralNetworkDeveloperClone {
     /// Create a [`NeuralNetwork`] out of a [`Genome`]
     ///
     /// [`NeuralNetwork`]: ../myelin-neural-network/trait.NeuralNetwork.html
     /// [`Genome`]: ./struct.Genome.html
     fn develop_neural_network(&self, genome: Genome) -> Box<dyn NeuralNetwork>;
+}
+
+/// Supertrait used to make sure that all implementors
+/// of [`NeuralNetworkDeveloper`] are [`Clone`]. You don't need
+/// to care about this type.
+///
+/// [`NeuralNetworkDeveloper`]: ./trait.NeuralNetworkDeveloper.html
+/// [`Clone`]: https://doc.rust-lang.org/nightly/std/clone/trait.Clone.html
+#[doc(hidden)]
+pub trait NeuralNetworkDeveloperClone {
+    fn clone_box(&self) -> Box<dyn NeuralNetworkDeveloper>;
+}
+
+impl<T> NeuralNetworkDeveloperClone for T
+where
+    T: NeuralNetworkDeveloper + Clone + 'static,
+{
+    default fn clone_box(&self) -> Box<dyn NeuralNetworkDeveloper> {
+        Box::new(self.clone())
+    }
+}
+
+impl Clone for Box<dyn NeuralNetworkDeveloper> {
+    fn clone(&self) -> Self {
+        self.clone_box()
+    }
 }
