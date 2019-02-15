56ced9ffd3c78d9c04713784487f0d07c1e19439
Jan Nils Ferner <jan@myelin.ch>
2019-02-15 13:11:28 +0100

    Add some genetic stubs

--- genetics/src/lib.rs
+++ genetics/src/lib.rs
@@ -7,3 +7,21 @@
     clippy::doc_markdown,
     clippy::unimplemented
 )]
+
+use myelin_neural_network::NeuralNetwork;
+
+/// The set of all genes in an organism
+#[derive(Debug)]
+pub struct Genome;
+
+/// A factory for producing a [`NeuralNetwork`] out of a [`Genome`]
+///
+/// [`NeuralNetwork`]: ../myelin-neural-network/trait.NeuralNetwork.html
+/// [`Genome`]: ./struct.Genome.html
+pub trait NeuralNetworkDeveloper {
+    /// Create a [`NeuralNetwork`] out of a [`Genome`]
+    ///
+    /// [`NeuralNetwork`]: ../myelin-neural-network/trait.NeuralNetwork.html
+    /// [`Genome`]: ./struct.Genome.html
+    fn develop_neural_network(genome: Genome) -> Box<dyn NeuralNetwork>;
+}
