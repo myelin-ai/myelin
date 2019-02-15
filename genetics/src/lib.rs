f9b4ca037355bb3a524c10ade1bfacdb841d7c12
Jan Nils Ferner <jan@myelin.ch>
2019-02-15 12:43:07 +0100

    Add empty genetics crate

---
+++ genetics/src/lib.rs
@@ -0,0 +1,9 @@
+//! Genes, genomes and the mechanisms needed to evolve neural networks from them
+
+#![deny(
+    rust_2018_idioms,
+    missing_debug_implementations,
+    missing_docs,
+    clippy::doc_markdown,
+    clippy::unimplemented
+)]
