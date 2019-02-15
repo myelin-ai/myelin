a272674f9626171738a69972fe2ac2a5f6f16773
Jan Nils Ferner <jan@myelin.ch>
2019-02-15 11:52:08 +0100

    Add constructor

--- object-behavior/src/organism.rs
+++ object-behavior/src/organism.rs
@@ -1,9 +1,19 @@
 use myelin_engine::prelude::*;
+use myelin_neural_network::NeuralNetwork;
 
 #[derive(Debug, Clone)]
 /// An organism that can interact with its surroundings via a neural network,
 /// built from a set of genes
-pub struct OrganismBehavior;
+pub struct OrganismBehavior {
+    neural_network: Box<dyn NeuralNetwork>,
+}
+
+impl OrganismBehavior {
+    /// Create a new `OrganismBehavior` with a `neural_network`
+    pub fn new(neural_network: Box<dyn NeuralNetwork>) -> Self {
+        Self { neural_network }
+    }
+}
 
 impl ObjectBehavior for OrganismBehavior {
     fn step(
