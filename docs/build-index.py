9921898a9c3cd941334cc3ecc58cae1a8d752f4a
Ruben Schmidmeister <ruben@myelin.ch>
2019-02-18 19:36:13 +0100

    Use data from `cargo metadata` for docs index

--- docs/build-index.py
+++ docs/build-index.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 from dataclasses import dataclass
-import toml
 import os
 from typing import List
 from pprint import pprint
 from string import Template
 from html import escape
+import subprocess
+import json
 
 _WORKSPACE_ROOT = os.path.join(os.path.dirname(__file__), '..')
 _CARGO_MANIFEST_FILE = 'Cargo.toml'
@@ -22,24 +23,27 @@ class Crate:
     description: str
 
 
-def _translate_package_name_to_crate_name(package_name: str) -> str:
-    return package_name.replace('-', '_')
+def _get_crates() -> List[Crate]:
+    cargo_metadata_string = subprocess.check_output(
+        ['cargo', 'metadata', '--no-deps', '--format-version', '1']).decode('utf-8')
+    cargo_metadata = json.loads(cargo_metadata_string)
+
+    crates = []
+    for package in cargo_metadata['packages']:
+        crates.append(_map_package_to_crate(package))
+    return crates
 
 
-def _extract_crate_metadata(crate_path: str) -> Crate:
-    manifest = toml.load(os.path.join(
-        _WORKSPACE_ROOT, crate_path, _CARGO_MANIFEST_FILE))
-    package = manifest['package']
+def _map_package_to_crate(package: dict) -> Crate:
     package_name = package['name']
     description = package['description']
     name = _translate_package_name_to_crate_name(package_name)
-    return Crate(name=name, package_name=package_name, description=description)
+    return Crate(name=name, package_name=package_name,
+                 description=description)
 
 
-def _get_workspace_members() -> List[str]:
-    workspace_manifest = toml.load(os.path.join(
-        _WORKSPACE_ROOT, _CARGO_MANIFEST_FILE))
-    return workspace_manifest['workspace']['members']
+def _translate_package_name_to_crate_name(package_name: str) -> str:
+    return package_name.replace('-', '_')
 
 
 def _get_crate_template() -> Template:
@@ -68,9 +72,7 @@ def _render(template: Template, rendered_crates: str) -> str:
 
 
 def build_index():
-    crates = [_extract_crate_metadata(crate)
-              for crate in _get_workspace_members()]
-
+    crates = _get_crates()
     rendered_crates = _render_crates(_get_crate_template(), crates)
     rendered = _render(_get_main_template(), rendered_crates)
 
