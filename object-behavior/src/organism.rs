39664fc367fc7d5dafe504a30b11955270d2d794
Jan Nils Ferner <jan@myelin.ch>
2019-03-01 16:26:10 +0100

    Cleanup input and output neuron mapping

--- object-behavior/src/organism.rs
+++ object-behavior/src/organism.rs
@@ -28,19 +28,17 @@ impl OrganismBehavior {
         parent_genomes: (Genome, Genome),
         neural_network_developer: Box<dyn NeuralNetworkDeveloper>,
     ) -> Self {
-        /// 1. Average acceleration since last step (x in positive direction)
-        /// 2. Average acceleration since last step (x in negative direction)
-        /// 3. Average acceleration since last step (y in positive direction)
-        /// 4. Average acceleration since last step (y in negative direction)
+        /// 1. Average linear acceleration since last step (forward)
+        /// 2. Average linear acceleration since last step (backward)
+        /// 3. Average angular acceleration since last step (right)
+        /// 4. Average angular acceleration since last step (left)
         const INPUT_NEURON_COUNT: usize = 4;
 
-        /// 1. Linear force x in positive direction
-        /// 2. Linear force x in negative direction
-        /// 3. Linear force y in positive direction
-        /// 4. Linear force y in negative direction
-        /// 5. Torque in positive direction
-        /// 6. Torque in negative direction
-        const OUTPUT_NEURON_COUNT: usize = 6;
+        /// 1. linear force (forward)
+        /// 2. linear force (backward)
+        /// 3. angular force (right)
+        /// 4. angular force (left)
+        const OUTPUT_NEURON_COUNT: usize = 4;
 
         let configuration = NeuralNetworkDevelopmentConfiguration {
             parent_genomes,
@@ -65,14 +63,7 @@ impl ObjectBehavior for OrganismBehavior {
     ) -> Option<Action> {
         let elapsed_time = world_interactor.elapsed_time_in_update().as_millis() as Milliseconds;
 
-        let input_neurons = &self.developed_neural_network.input_neuron_handles;
-        let output_neurons = &self.developed_neural_network.output_neuron_handles;
-        let neural_network = &mut self.developed_neural_network.neural_network;
-
-        let acceleration_x_positive_input_neuron = get_neuron_handle(0, input_neurons);
-        let acceleration_x_negative_input_neuron = get_neuron_handle(1, input_neurons);
-        let acceleration_y_positive_input_neuron = get_neuron_handle(2, input_neurons);
-        let acceleration_y_negative_input_neuron = get_neuron_handle(3, input_neurons);
+        let neuron_handle_mapping = map_handles(&self.developed_neural_network);
 
         let current_velocity = match own_description.mobility {
             Mobility::Immovable => Vector::default(),
@@ -84,64 +75,57 @@ impl ObjectBehavior for OrganismBehavior {
         let mut inputs = HashMap::with_capacity(2);
 
         inputs.insert(
+            // Todo: Replace .x by forward linear acceleration
             if acceleration.x >= 0.0 {
-                acceleration_x_positive_input_neuron
+                neuron_handle_mapping.input.linear_acceleration.forward
             } else {
-                acceleration_x_negative_input_neuron
+                neuron_handle_mapping.input.linear_acceleration.backward
             },
             acceleration.x,
         );
 
         inputs.insert(
+            // Todo: Replace .y by right angular acceleration
             if acceleration.y >= 0.0 {
-                acceleration_y_positive_input_neuron
+                neuron_handle_mapping.input.angular_acceleration.right
             } else {
-                acceleration_y_negative_input_neuron
+                neuron_handle_mapping.input.angular_acceleration.left
             },
             acceleration.y,
         );
 
         self.previous_velocity = current_velocity;
 
+        let neural_network = &mut self.developed_neural_network.neural_network;
         neural_network.step(
             world_interactor.elapsed_time_in_update().as_millis() as Milliseconds,
             &inputs,
         );
 
-        let linear_force_x_positive_output_neuron = get_neuron_handle(0, output_neurons);
-        let linear_force_x_negative_output_neuron = get_neuron_handle(1, output_neurons);
-        let linear_force_y_positive_output_neuron = get_neuron_handle(2, output_neurons);
-        let linear_force_y_negative_output_neuron = get_neuron_handle(3, output_neurons);
-        let torque_positive_output_neuron = get_neuron_handle(4, output_neurons);
-        let torque_negative_output_neuron = get_neuron_handle(5, output_neurons);
-
-        let linear_force_x = get_normalized_potential(
-            linear_force_x_positive_output_neuron,
+        let linear_force = get_normalized_potential(
+            neuron_handle_mapping.output.linear_acceleration.forward,
             neural_network.as_ref(),
         ) + get_normalized_potential(
-            linear_force_x_negative_output_neuron,
+            neuron_handle_mapping.output.linear_acceleration.backward,
             neural_network.as_ref(),
         );
 
-        let linear_force_y = get_normalized_potential(
-            linear_force_y_positive_output_neuron,
+        let angular_force = get_normalized_potential(
+            neuron_handle_mapping.output.angular_acceleration.right,
             neural_network.as_ref(),
         ) + get_normalized_potential(
-            linear_force_y_negative_output_neuron,
+            neuron_handle_mapping.output.angular_acceleration.left,
             neural_network.as_ref(),
         );
 
-        let torque =
-            get_normalized_potential(torque_positive_output_neuron, neural_network.as_ref())
-                + get_normalized_potential(torque_negative_output_neuron, neural_network.as_ref());
-
-        if linear_force_x != 0.0 || linear_force_y != 0.0 || torque != 0.0 {
+        if linear_force != 0.0 || angular_force != 0.0 {
             Some(Action::ApplyForce(Force {
+                // Todo: Translate forward linear force to global linear force
                 linear: Vector {
                     x: linear_force_x,
                     y: linear_force_y,
                 },
-                torque: Torque(torque),
+                torque: Torque(angular_force),
             }))
         } else {
             None
@@ -150,6 +134,64 @@ impl ObjectBehavior for OrganismBehavior {
 
     fn as_any(&self) -> &dyn Any {
         self
+    }
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+struct NeuronHandleMapping {
+    input: InputNeuronHandleMapping,
+    output: OutputNeuronHandleMapping,
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+struct InputNeuronHandleMapping {
+    linear_acceleration: LinearAccelerationHandleMapping,
+    angular_acceleration: AngularAccelerationHandleMapping,
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+struct LinearAccelerationHandleMapping {
+    forward: Handle,
+    backward: Handle,
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+struct AngularAccelerationHandleMapping {
+    left: Handle,
+    right: Handle,
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+struct OutputNeuronHandleMapping {
+    linear_acceleration: LinearAccelerationHandleMapping,
+    angular_acceleration: AngularAccelerationHandleMapping,
+}
+
+fn map_handles(developed_neural_network: &DevelopedNeuralNetwork) -> NeuronHandleMapping {
+    let input_neurons = &developed_neural_network.input_neuron_handles;
+    let output_neurons = &developed_neural_network.output_neuron_handles;
+
+    NeuronHandleMapping {
+        input: InputNeuronHandleMapping {
+            linear_acceleration: LinearAccelerationHandleMapping {
+                forward: get_neuron_handle(0, input_neurons),
+                backward: get_neuron_handle(1, input_neurons),
+            },
+            angular_acceleration: AngularAccelerationHandleMapping {
+                left: get_neuron_handle(2, input_neurons),
+                right: get_neuron_handle(3, input_neurons),
+            },
+        },
+        output: OutputNeuronHandleMapping {
+            linear_acceleration: LinearAccelerationHandleMapping {
+                forward: get_neuron_handle(0, output_neurons),
+                backward: get_neuron_handle(1, output_neurons),
+            },
+            angular_acceleration: AngularAccelerationHandleMapping {
+                left: get_neuron_handle(2, output_neurons),
+                right: get_neuron_handle(3, output_neurons),
+            },
+        },
     }
 }
 
