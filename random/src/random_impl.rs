096f70b51fb5be6f99c84f4c91e94f9fe627122a
Jan Nils Ferner <jan@myelin.ch>
2019-03-25 14:16:35 +0100

    Use random immutably

--- random/src/random_impl.rs
+++ random/src/random_impl.rs
@@ -51,56 +51,56 @@ mod tests {
 
     #[test]
     fn flips_true_on_100_percent_chance() {
-        let mut random = RandomImpl::new();
+        let random = RandomImpl::new();
         let coin_flip_result = random.flip_coin_with_probability(1.0);
         assert!(coin_flip_result);
     }
 
     #[test]
     fn flips_false_on_0_percent_chance() {
-        let mut random = RandomImpl::new();
+        let random = RandomImpl::new();
         let coin_flip_result = random.flip_coin_with_probability(0.0);
         assert!(!coin_flip_result);
     }
 
     #[test]
     fn does_not_panic_on_valid_range() {
-        let mut random = RandomImpl::new();
+        let random = RandomImpl::new();
         random.flip_coin_with_probability(0.5);
     }
 
     #[should_panic]
     #[test]
     fn panics_on_negative_probability() {
-        let mut random = RandomImpl::new();
+        let random = RandomImpl::new();
         random.flip_coin_with_probability(-1.0);
     }
 
     #[should_panic]
     #[test]
     fn panics_too_high_probability() {
-        let mut random = RandomImpl::new();
+        let random = RandomImpl::new();
         random.flip_coin_with_probability(1.1);
     }
 
     #[should_panic]
     #[test]
     fn panics_when_min_is_higher_than_max() {
-        let mut random = RandomImpl::default();
+        let random = RandomImpl::default();
         random.random_number_in_range(1, 0);
     }
 
     #[should_panic]
     #[test]
     fn panics_when_min_is_max() {
-        let mut random = RandomImpl::default();
+        let random = RandomImpl::default();
         const ONLY_BOUND: i32 = 1;
         random.random_number_in_range(ONLY_BOUND, ONLY_BOUND);
     }
 
     #[test]
     fn returned_number_is_in_range() {
-        let mut random = RandomImpl::default();
+        let random = RandomImpl::default();
         const MIN: i32 = -1;
         const MAX: i32 = 3;
 
@@ -110,7 +110,7 @@ mod tests {
 
     #[test]
     fn returns_only_possibility() {
-        let mut random = RandomImpl::default();
+        let random = RandomImpl::default();
         const MIN: i32 = 1;
         const MAX: i32 = 2;
 
