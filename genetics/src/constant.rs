edde820a674601c36ff9ffd5818bde1117e4ae67
Ruben Schmidmeister <ruben@myelin.ch>
2019-03-25 16:30:30 +0100

    Document constant

--- genetics/src/constant.rs
+++ genetics/src/constant.rs
@@ -1,3 +1,4 @@
 //! Hyperparameters
 
+/// Probabilty of an extra gene (only found in one of the two genomes) being used during crossover.
 pub(crate) const CROSSOVER_EXTRA_GENE_SELECTION_PROBABILITY: f64 = 0.75;
