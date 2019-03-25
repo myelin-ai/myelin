06d783742ba25ad687eb27d298b0570f648decc3
Jeremy Stucki <jeremy@myelin.ch>
2019-03-25 12:36:03 +0100

    Add test

--- genetics/src/deriver/crossover.rs
+++ genetics/src/deriver/crossover.rs
@@ -78,7 +78,7 @@ mod tests {
     }
 
     #[test]
-    fn derive_genome_from_parents_with_different_length() {
+    fn derive_genome_from_parents_with_left_being_longer() {
         let genome_one = Genome {
             hox_genes: vec![hox_gene(0), hox_gene(1)],
             cluster_genes: vec![cluster_gene(2), cluster_gene(3), cluster_gene(4)],
@@ -100,6 +100,37 @@ mod tests {
         random_chance_checker.expect_flip_coin().returns(false);
         random_chance_checker.expect_flip_coin_with_probability(any()).returns(false);
         random_chance_checker.expect_flip_coin_with_probability(any()).returns(true);
+
+        let mut deriver = ChromosomalCrossover::new(box random_chance_checker);
+
+        let actual_genome = deriver.derive_genome_from_parents((genome_one, genome_two));
+
+        assert_eq!(expected_genome, actual_genome);
+    }
+
+    #[test]
+    fn derive_genome_from_parents_with_right_being_longer() {
+        let genome_one = Genome {
+            hox_genes: vec![hox_gene(0), hox_gene(1)],
+            cluster_genes: vec![cluster_gene(2)],
+        };
+
+        let genome_two = Genome {
+            hox_genes: vec![hox_gene(10), hox_gene(11)],
+            cluster_genes: vec![cluster_gene(12), cluster_gene(13), cluster_gene(14)],
+        };
+
+        let expected_genome = Genome {
+            hox_genes: vec![hox_gene(0), hox_gene(11)],
+            cluster_genes: vec![cluster_gene(12), cluster_gene(13)],
+        };
+
+        let mut random_chance_checker = RandomChanceCheckerMock::default();
+        random_chance_checker.expect_flip_coin().returns(true);
+        random_chance_checker.expect_flip_coin().returns(false);
+        random_chance_checker.expect_flip_coin().returns(false);
+        random_chance_checker.expect_flip_coin_with_probability(any()).returns(true);
+        random_chance_checker.expect_flip_coin_with_probability(any()).returns(false);
 
         let mut deriver = ChromosomalCrossover::new(box random_chance_checker);
 
