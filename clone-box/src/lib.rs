dc3c40ae528f4ca879c2e1444a741c6c047fdbe1
Ruben Schmidmeister <ruben@myelin.ch>
2019-03-25 14:45:06 +0100

    Add lints

--- clone-box/src/lib.rs
+++ clone-box/src/lib.rs
@@ -1,3 +1,16 @@
+#![warn(missing_docs, clippy::dbg_macro, clippy::unimplemented)]
+#![deny(
+    rust_2018_idioms,
+    future_incompatible,
+    missing_debug_implementations,
+    clippy::doc_markdown,
+    clippy::default_trait_access,
+    clippy::enum_glob_use,
+    clippy::needless_borrow,
+    clippy::large_digit_groups,
+    clippy::explicit_into_iter_loop
+)]
+
 #[macro_export]
 macro_rules! clone_box {
     ($trait_ident: ident, $clone_trait_ident: ident) => {
@@ -25,5 +38,5 @@ macro_rules! clone_box {
                 self.clone_box()
             }
         }
-    }
+    };
 }
