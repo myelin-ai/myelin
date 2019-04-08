0518cb46989c476a2a98f2b88193286602a879f5
Jan Nils Ferner <jan@myelin.ch>
2019-04-08 15:55:02 +0200

    Add test for hox placing first cluster on first cluster

--- genetics/src/developer/genetic.rs
+++ genetics/src/developer/genetic.rs
@@ -34,8 +34,8 @@ impl NeuralNetworkDeveloper for GeneticNeuralNetworkDeveloper {
 mod tests {
     use super::*;
     use crate::genome::{
-        ClusterGene, ClusterGeneIndex, Connection as ConnectionDefinition, HoxGene, HoxPlacement,
-        Neuron, NeuronClusterLocalIndex,
+        ClusterGene, ClusterGeneIndex, Connection as ConnectionDefinition, HoxGene, HoxGeneIndex,
+        HoxPlacement, Neuron, NeuronClusterLocalIndex,
     };
     use crate::orchestrator_impl::NeuralNetworkConfiguratorMock;
     use mockiato::partial_eq;
@@ -81,6 +81,26 @@ mod tests {
         developer.develop_neural_network(&mut configurator);
     }
 
+    #[test]
+    fn places_hox() {
+        let genome = genome_stub();
+        let genome = add_first_cluster_to_genome(genome);
+
+        let genome = add_initial_hox_gene_to_genome(genome);
+        let genome = add_hox_gene_placing_first_cluster_on_first_hox_clusters(genome);
+
+        let config = config_stub();
+
+        let developer = box GeneticNeuralNetworkDeveloper::new(config, genome);
+        let mut configurator = NeuralNetworkConfiguratorMock::new();
+
+        expect_push_amount_of_neurons(&mut configurator, 7);
+        expect_first_cluster_connections(&mut configurator);
+        expect_first_cluster_placed_on_first_cluster_connections(&mut configurator);
+
+        developer.develop_neural_network(&mut configurator);
+    }
+
     fn expect_first_cluster_connections(configurator: &mut NeuralNetworkConfiguratorMock<'_>) {
         first_cluster_connections()
             .into_iter()
@@ -101,7 +121,26 @@ mod tests {
                 connection_definition_to_placed_connection(ConnectionTranslationParameters {
                     connection: connection_definition,
                     offset: 4,
-                    placement_neuron_index: 0,
+                    placement_neuron_index: 1,
+                })
+            })
+            .for_each(|connection| {
+                configurator
+                    .expect_add_connection(partial_eq(connection))
+                    .returns(Ok(()));
+            });
+    }
+
+    fn expect_first_cluster_placed_on_first_cluster_connections(
+        configurator: &mut NeuralNetworkConfiguratorMock<'_>,
+    ) {
+        first_cluster_connections()
+            .into_iter()
+            .map(|connection_definition| {
+                connection_definition_to_placed_connection(ConnectionTranslationParameters {
+                    connection: connection_definition,
+                    offset: 4,
+                    placement_neuron_index: 1,
                 })
             })
             .for_each(|connection| {
@@ -273,6 +312,21 @@ mod tests {
                     target_neuron: NeuronClusterLocalIndex(2),
                 },
                 cluster_index: ClusterGeneIndex(1),
+                disabled_connections: Vec::new(),
+            },
+        );
+        genome
+    }
+
+    fn add_hox_gene_placing_first_cluster_on_first_hox_clusters(mut genome: Genome) -> Genome {
+        genome.hox_genes.insert(
+            1,
+            HoxGene {
+                placement: HoxPlacement::HoxGene {
+                    hox_gene: HoxGeneIndex(0),
+                    target_neuron: NeuronClusterLocalIndex(3),
+                },
+                cluster_index: ClusterGeneIndex(0),
                 disabled_connections: Vec::new(),
             },
         );
