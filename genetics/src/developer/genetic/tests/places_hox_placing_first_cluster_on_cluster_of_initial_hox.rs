44edde6fe9e996f4d840c5a751ededd49ec9ddc9
Jan Nils Ferner <jan@myelin.ch>
2019-04-15 11:02:59 +0200

    Remove hox duplication

--- genetics/src/developer/genetic/tests/places_hox_placing_first_cluster_on_cluster_of_initial_hox.rs
+++ genetics/src/developer/genetic/tests/places_hox_placing_first_cluster_on_cluster_of_initial_hox.rs
@@ -5,8 +5,8 @@ fn places_hox_placing_first_cluster_on_cluster_of_initial_hox() {
     let genome = genome_stub();
     let genome = add_first_cluster_to_genome(genome);
 
-    let genome = add_initial_hox_gene_to_genome(genome);
-    let genome = add_hox_gene_placing_first_cluster_on_first_hox_clusters(genome);
+    let mut genome = add_initial_hox_gene_to_genome(genome);
+    add_hox_gene_placing_first_cluster_on_first_hox_clusters(&mut genome);
 
     let config = config_stub();
 
@@ -20,19 +20,15 @@ fn places_hox_placing_first_cluster_on_cluster_of_initial_hox() {
     developer.develop_neural_network(&mut configurator);
 }
 
-fn add_hox_gene_placing_first_cluster_on_first_hox_clusters(mut genome: Genome) -> Genome {
-    genome.hox_genes.insert(
-        1,
-        HoxGene {
-            placement: HoxPlacement::HoxGene {
-                hox_gene: HoxGeneIndex(0),
-                target_neuron: NeuronClusterLocalIndex(3),
-            },
+fn add_hox_gene_placing_first_cluster_on_first_hox_clusters(genome: &mut Genome) {
+    add_hox_gene_placing_cluster_on_hox(
+        genome,
+        ClusterOnHoxTestParameters {
+            hox_gene: HoxGeneIndex(0),
+            target_neuron: NeuronClusterLocalIndex(3),
             cluster_index: ClusterGeneIndex(0),
-            disabled_connections: Vec::new(),
         },
-    );
-    genome
+    )
 }
 
 fn expect_first_cluster_placed_on_first_cluster_connections(
