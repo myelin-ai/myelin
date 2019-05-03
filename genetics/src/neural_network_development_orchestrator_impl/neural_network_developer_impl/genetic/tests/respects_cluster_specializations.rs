56144cdeec7d09b7c6907b015629a6e63f0dbd0d
Jan Nils Ferner <jan@myelin.ch>
2019-05-03 13:58:33 +0200

    Start writing input cluster test

--- genetics/src/neural_network_development_orchestrator_impl/neural_network_developer_impl/genetic/tests/respects_cluster_specializations.rs
+++ genetics/src/neural_network_development_orchestrator_impl/neural_network_developer_impl/genetic/tests/respects_cluster_specializations.rs
@@ -19,7 +19,22 @@ fn cluster_with_initial_marker_can_be_placed() {
 }
 
 fn cluster_with_input_neuron_marker_can_be_attached_to_initial_cluster() {
-    unimplemented!()
+    let genome = GenomeStubBuilder::new()
+        .add_first_cluster_marked_as_initial_cluster()
+        .add_second_cluster_marked_as_input_cluster()
+        .add_initial_hox_gene()
+        .add_hox_gene_placing_second_cluster_on_first_cluster()
+        .build();
+
+    let config = config_stub();
+
+    let developer = box GeneticNeuralNetworkDeveloper::new(config, genome);
+    let mut configurator = NeuralNetworkConfiguratorMock::new();
+
+    expect_push_amount_of_neurons(&mut configurator, 4);
+    expect_first_cluster_placed_standalone(&mut configurator, 0);
+
+    developer.develop_neural_network(&mut configurator);
 }
 
 fn input_neuron_marker_is_respected() {
