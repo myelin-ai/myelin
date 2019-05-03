cd01598ca26d61f17581b14ab6a0de32848eb62d
Jan Nils Ferner <jan@myelin.ch>
2019-05-03 14:41:39 +0200

    Implement output cluster test

--- genetics/src/neural_network_development_orchestrator_impl/neural_network_developer_impl/genetic/tests/respects_cluster_specializations.rs
+++ genetics/src/neural_network_development_orchestrator_impl/neural_network_developer_impl/genetic/tests/respects_cluster_specializations.rs
@@ -41,22 +41,27 @@ fn cluster_with_input_neuron_marker_can_be_attached_to_initial_cluster() {
     developer.develop_neural_network(&mut configurator);
 }
 
-#[ignore]
-#[test]
-fn input_neuron_marker_is_respected() {
-    unimplemented!()
-}
-
-#[ignore]
 #[test]
 fn cluster_with_output_neuron_marker_can_be_attached_to_initial_cluster() {
-    unimplemented!()
-}
+    let genome = GenomeStubBuilder::new()
+        .add_first_cluster_marked_as_initial_cluster()
+        .add_second_cluster_marked_as_output_cluster()
+        .add_initial_hox_gene()
+        .add_hox_gene_placing_second_cluster_on_first_cluster()
+        .build();
 
-#[ignore]
-#[test]
-fn output_neuron_marker_is_respected() {
-    unimplemented!()
+    let config = config_stub();
+
+    let developer = box GeneticNeuralNetworkDeveloper::new(config, genome);
+    let mut configurator = NeuralNetworkConfiguratorMock::new();
+
+    expect_push_amount_of_neurons(&mut configurator, 5);
+    expect_push_amount_of_output_neurons(&mut configurator, 1);
+
+    expect_first_cluster_placed_standalone(&mut configurator, 0);
+    expect_second_cluster_placed_on_first_cluster_connections(&mut configurator);
+
+    developer.develop_neural_network(&mut configurator);
 }
 
 impl GenomeStubBuilder {
@@ -65,6 +70,12 @@ impl GenomeStubBuilder {
     }
 
     fn add_second_cluster_marked_as_input_cluster(&mut self) -> &mut Self {
+        self.add_second_cluster_with_specialization(ClusterGeneSpecilization::Input(
+            NeuronClusterLocalIndex(2),
+        ))
+    }
+
+    fn add_second_cluster_marked_as_output_cluster(&mut self) -> &mut Self {
         self.add_second_cluster_with_specialization(ClusterGeneSpecilization::Input(
             NeuronClusterLocalIndex(2),
         ))
