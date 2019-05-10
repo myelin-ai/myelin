117e911dd35d8cbf09ebbbb2998d4af42aaec4db
Ruben Schmidmeister <ruben@myelin.ch>
2019-05-10 15:56:47 +0200

    Include stem neurons

--- genetics/src/genome_generator_impl/corpus_callosum_cluster_gene_generator_impl.rs
+++ genetics/src/genome_generator_impl/corpus_callosum_cluster_gene_generator_impl.rs
@@ -17,8 +17,10 @@ impl CorpusCallosumClusterGeneGeneratorImpl {
 
 impl CorpusCallosumClusterGeneGenerator for CorpusCallosumClusterGeneGeneratorImpl {
     fn generate_cluster_gene(&self, configuration: &CorpusCallosumConfiguration) -> ClusterGene {
-        let neuron_count =
-            configuration.input_neuron_count.get() + configuration.output_neuron_count.get();
+        let output_neuron_count = configuration.output_neuron_count.get();
+        let input_neuron_count = configuration.input_neuron_count.get();
+        let stem_neuron_count = self.random.random_usize_in_range(MIN_STEM_NEURONS, output_neuron_count); 
+        let neuron_count = input_neuron_count + output_neuron_count + stem_neuron_count;
         ClusterGene {
             neurons: vec![Neuron {}; neuron_count],
             connections: Vec::new(),
@@ -28,6 +30,7 @@ impl CorpusCallosumClusterGeneGenerator for CorpusCallosumClusterGeneGeneratorIm
     }
 }
 
+const MIN_STEM_NEURONS: usize = 1;
 /// Chosen arbitrarily
 const MIN_CONNECTION_WEIGHT: f64 = 0.000_000_1;
 const MAX_CONNECTION_WEIGHT: f64 = 1.0;
@@ -46,6 +49,8 @@ mod tests {
     fn generates_correct_amount_of_neurons_and_connections() {
         const INPUT_NEURON_COUNT: usize = 5;
         const OUTPUT_NEURON_COUNT: usize = 2;
+        const STEM_NEURONS: usize = 5;
+        const NEURONS: usize = INPUT_NEURON_COUNT + OUTPUT_NEURON_COUNT + STEM_NEURONS;
         let random = {
             let mut random = RandomMock::new();
             random
@@ -55,13 +60,19 @@ mod tests {
                 )
                 .times(..)
                 .returns(1.0);
+            random
+                .expect_random_usize_in_range(
+                    partial_eq(MIN_STEM_NEURONS),
+                    partial_eq(OUTPUT_NEURON_COUNT),
+                )
+                .returns(STEM_NEURONS);
             box random
         };
         let generator = CorpusCallosumClusterGeneGeneratorImpl::new(random);
         let configuration = configuration(INPUT_NEURON_COUNT, OUTPUT_NEURON_COUNT);
-        let expected_neurons = vec![Neuron {}; INPUT_NEURON_COUNT + OUTPUT_NEURON_COUNT];
+        let expected_neurons = vec![Neuron {}; NEURONS];
         let generated_cluster_gene = generator.generate_cluster_gene(&configuration);
-        assert_eq!(expected_neurons, generated_cluster_gene.neurons,);
+        assert_eq!(expected_neurons, generated_cluster_gene.neurons);
     }
 
     fn configuration(
