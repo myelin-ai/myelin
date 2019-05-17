7458045631e904aee2e3cac91c249cee0d8306e4
Ruben Schmidmeister <ruben@myelin.ch>
2019-05-17 14:25:37 +0200

    Prepare test setup for genome generator

--- genetics/src/genome_generator_impl.rs
+++ genetics/src/genome_generator_impl.rs
@@ -41,11 +41,70 @@ pub struct CorpusCallosumConfiguration {
 }
 
 /// Default implementation of [`GenomeGenerator`].
-#[derive(Debug, Default, Clone)]
-pub struct GenomeGeneratorImpl;
+#[derive(Debug)]
+pub struct GenomeGeneratorImpl {
+    io_cluster_gene_generator: Box<dyn IoClusterGeneGenerator>,
+    corpus_callosum_cluster_gene_generator: Box<dyn CorpusCallosumClusterGeneGenerator>,
+}
+
+impl GenomeGeneratorImpl {
+    pub fn new(
+        io_cluster_gene_generator: Box<dyn IoClusterGeneGenerator>,
+        corpus_callosum_cluster_gene_generator: Box<dyn CorpusCallosumClusterGeneGenerator>,
+    ) -> Self {
+        GenomeGeneratorImpl {
+            io_cluster_gene_generator,
+            corpus_callosum_cluster_gene_generator,
+        }
+    }
+}
 
 impl GenomeGenerator for GenomeGeneratorImpl {
     fn generate_genome(&self, _configuration: &GenomeGeneratorConfiguration) -> Genome {
         unimplemented!();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn generates_correct_genome() {
+        test_genome_is_generated_correctly(GenerateGenomeTestConfiguration {
+            input_neuron_count: 7,
+            output_neuron_count: 4,
+        });
+    }
+
+    struct GenerateGenomeTestConfiguration {
+        input_neuron_count: usize,
+        output_neuron_count: usize,
+    }
+
+    fn test_genome_is_generated_correctly(
+        GenerateGenomeTestConfiguration {
+            input_neuron_count,
+            output_neuron_count,
+        }: GenerateGenomeTestConfiguration,
+    ) {
+        let config = genome_generator_configuration(input_neuron_count, output_neuron_count);
+        let mut io_cluster_gene_generator = IoClusterGeneGeneratorMock::new();
+        let mut corpus_callosum_cluster_gene_generator = CorpusCallosumClusterGeneGeneratorMock::new();
+        let genome_generator = GenomeGeneratorImpl::new(
+            box io_cluster_gene_generator,
+            box corpus_callosum_cluster_gene_generator,
+        );
+        let _genome = genome_generator.generate_genome(&config);
+    }
+
+    fn genome_generator_configuration(
+        input_neuron_count: usize,
+        output_neuron_count: usize,
+    ) -> GenomeGeneratorConfiguration {
+        GenomeGeneratorConfiguration {
+            input_neuron_count: NonZeroUsize::new(input_neuron_count).unwrap(),
+            output_neuron_count: NonZeroUsize::new(output_neuron_count).unwrap(),
+        }
     }
 }
