7c5513ab73b61e46ef89311da9a44ae4348a965d
Jan Nils Ferner <jan@myelin.ch>
2019-05-17 13:17:21 +0200

    Add mutation variants
    Co-authored-by: Mathias Fischler <mathias@myelin.ch>

--- genetics/src/neural_network_development_orchestrator_impl/genome_mutator_impl/variants.rs
+++ genetics/src/neural_network_development_orchestrator_impl/genome_mutator_impl/variants.rs
@@ -1,15 +1,60 @@
-pub enum MutationVariant {
-    HoxMutationVariant(HoxMutationVariant),
-    ClusterMutationVariant(ClusterMutationVariant),
-}
+use crate::genome::{
+    ClusterGeneIndex, Connection, HoxGene, HoxPlacement, NeuronClusterLocalIndex, Weight,
+};
 
-pub enum HoxMutationVariant {
-    CopySelf,
-}
-
-pub enum ClusterUnitMutationVariant {}
-
-pub enum ClusterMutationVariant {
-    ShiftWeight,
-    Bridge,
+/// The possible genotypic mutations
+#[derive(Debug, Clone, PartialEq)]
+pub enum MutationVariants {
+    AddNeuron {
+        cluster: ClusterGeneIndex,
+        connection_index: usize,
+    },
+    AddConnection {
+        cluster: ClusterGeneIndex,
+        connection: Connection,
+    },
+    DisableConnection {
+        hox_index: usize,
+        connection_filter: ConnectionFilter,
+    },
+    NudgeWeight {
+        cluster: ClusterGeneIndex,
+        connection_index: usize,
+        weight_delta: Weight,
+    },
+    RemoveNeuron {
+        cluster: ClusterGeneIndex,
+        neuron: NeuronClusterLocalIndex,
+    },
+    ChangePlacementNeuron {
+        cluster: ClusterGeneIndex,
+        new_placement_neuron: NeuronClusterLocalIndex,
+    },
+    AddCluster {
+        new_cluster: ClusterGene,
+        new_hox: HoxGene,
+    },
+    CopyCluster {
+        source_cluster: ClusterGeneIndex,
+        new_hox: HoxGene,
+    },
+    DesyncCluster {
+        cluster: ClusterGeneIndex,
+        hox_index: usize,
+    },
+    Bridge {
+        source_cluster: ClusterGeneIndex,
+        target_cluster: ClusterGeneIndex,
+        bridge_cluster: ClusterGene,
+    },
+    AddHoxWithExistingCluster {
+        new_hox: HoxGene,
+    },
+    ChangeTargetNeuron {
+        hox_index: usize,
+        new_target_neuron: NeuronClusterLocalIndex,
+    },
+    DuplicateHox {
+        hox_index: usize,
+    },
 }
