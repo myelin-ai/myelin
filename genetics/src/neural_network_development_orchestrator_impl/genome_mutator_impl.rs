503f8761417a1920339b7e73d05caaf556da64fa
Ruben Schmidmeister <ruben@myelin.ch>
2019-05-27 14:53:39 +0200

    Rename MutationVariants → Mutation

--- genetics/src/neural_network_development_orchestrator_impl/genome_mutator_impl.rs
+++ genetics/src/neural_network_development_orchestrator_impl/genome_mutator_impl.rs
@@ -1,18 +1,18 @@
 //! Trait and implementations for mutating [`Genome`]s.
 
 use crate::genome::Genome;
-pub use self::variants::*;
+pub use self::mutation::*;
 
-mod variants;
+mod mutation;
 
-/// Generates a random [`MutationVariants`].
+/// Generates a random [`Mutation`].
 pub trait MutationGenerator {
-    /// Picks a variant of [`MutationVariants`] at random and generates it.
-    fn generate_mutation(&self, genome: &Genome) -> MutationVariants;
+    /// Picks a variant of [`Mutation`] at random and generates it.
+    fn generate_mutation(&self, genome: &Genome) -> Mutation;
 }
 
 /// Applies mutations to a [`Genome`].
 pub trait MutationApplier {
-    /// Applies the given [`MutationVariant`] to a [`Genome`].
-    fn apply_mutation(&self, genome: &mut Genome, mutation: MutationVariants);
+    /// Applies the given [`Mutation`] to a [`Genome`].
+    fn apply_mutation(&self, genome: &mut Genome, mutation: Mutation);
 }
