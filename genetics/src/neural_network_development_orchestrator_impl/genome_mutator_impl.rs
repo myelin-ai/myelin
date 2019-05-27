e6c9be42b0676d4c820d19cc9739092637fad5d7
Ruben Schmidmeister <ruben@myelin.ch>
2019-05-27 14:51:09 +0200

    Define traits for mutation generation and application

--- genetics/src/neural_network_development_orchestrator_impl/genome_mutator_impl.rs
+++ genetics/src/neural_network_development_orchestrator_impl/genome_mutator_impl.rs
@@ -1,4 +1,18 @@
 //! Trait and implementations for mutating [`Genome`]s.
 
+use crate::genome::Genome;
 pub use self::variants::*;
+
 mod variants;
+
+/// Generates a random [`MutationVariants`].
+pub trait MutationGenerator {
+    /// Picks a variant of [`MutationVariants`] at random and generates it.
+    fn generate_mutation(&self, genome: &Genome) -> MutationVariants;
+}
+
+/// Applies mutations to a [`Genome`].
+pub trait MutationApplier {
+    /// Applies the given [`MutationVariant`] to a [`Genome`].
+    fn apply_mutation(&self, genome: &mut Genome, mutation: MutationVariants);
+}
