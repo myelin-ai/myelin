27eef183bed8ed34fca72eb57eaf0a5da97a1380
Ruben Schmidmeister <ruben@myelin.ch>
2019-05-31 14:08:25 +0200

    Setup test API

--- genetics/src/neural_network_development_orchestrator_impl/genome_mutator_impl/mutation_applier_impl/tests.rs
+++ genetics/src/neural_network_development_orchestrator_impl/genome_mutator_impl/mutation_applier_impl/tests.rs
@@ -1,8 +1,63 @@
+use super::super::Mutation;
 use super::*;
 
-fn genome() -> Genome {
+struct MutationApplicationTestParameters<F> {
+    genome: Genome,
+    expected_genome: Genome,
+    mutation: Mutation,
+    result_test_fn: F,
+}
+
+fn test_mutation_application<F>(
+    MutationApplicationTestParameters {
+        mut genome,
+        expected_genome,
+        mutation,
+        result_test_fn,
+    }: MutationApplicationTestParameters<F>,
+) where
+    F: for<'a> FnOnce(&'a Result<(), Box<dyn Error>>) -> bool,
+{
+    let mutation_applier = MutationApplierImpl::new();
+    let result = mutation_applier.apply_mutation(&mut genome, mutation);
+    assert!(result_test_fn(&result));
+    assert_eq!(genome, expected_genome);
+}
+
+fn empty_genome() -> Genome {
     Genome {
-        hox_genes: vec![],
-        cluster_genes: vec![],
+        hox_genes: Vec::new(),
+        cluster_genes: Vec::new(),
+    }
+}
+
+impl Genome {
+    fn add_cluster_gene(mut self, cluster_gene: ClusterGene) -> Self {
+        self.cluster_genes.push(cluster_gene);
+        self
+    }
+
+    fn add_first_cluster_gene(self) -> Self {
+        self.add_cluster_gene(first_cluster_gene())
+    }
+}
+
+fn first_cluster_gene() -> ClusterGene {
+    ClusterGene {
+        neurons: vec![Neuron {}; 2],
+        placement_neuron: ClusterNeuronIndex(0),
+        specialization: ClusterGeneSpecialization::None,
+        connections: vec![
+            Connection {
+                from: ClusterNeuronIndex(0),
+                to: ClusterNeuronIndex(1),
+                weight: 1.0,
+            },
+            Connection {
+                from: ClusterNeuronIndex(1),
+                to: ClusterNeuronIndex(0),
+                weight: 1.0,
+            },
+        ],
     }
 }
