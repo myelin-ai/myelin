e4982f85570acf8225e07abcfc8ae0c989d7bf8f
Jan Nils Ferner <jan@myelin.ch>
2019-06-07 14:17:47 +0200

    Add test for bridge with additional connected neurons

--- genetics/src/neural_network_development_orchestrator_impl/genome_mutator_impl/mutation_applier_impl/tests/bridge.rs
+++ genetics/src/neural_network_development_orchestrator_impl/genome_mutator_impl/mutation_applier_impl/tests/bridge.rs
@@ -49,7 +49,55 @@ fn adds_single_bridge() {
 
 #[test]
 fn adds_bridge_to_neuron_with_multiple_attached_clusters() {
-    unimplemented!()
+    let initial_target_neuron_on_mutation_target = ClusterNeuronIndex(2);
+    let base_genome = empty_genome()
+        .add_cluster_gene(cluster_gene())
+        .add_cluster_gene(cluster_gene())
+        .add_hox_gene(standalone_hox_gene(ClusterGeneIndex(0)));
+
+    let genome = base_genome.clone().add_hox_gene(hox_placed_on_hox_gene(
+        ClusterGeneIndex(1),
+        HoxGeneIndex(0),
+        initial_target_neuron_on_mutation_target,
+    ));
+
+    let genome = base_genome.clone().add_hox_gene(hox_placed_on_hox_gene(
+        ClusterGeneIndex(1),
+        HoxGeneIndex(0),
+        initial_target_neuron_on_mutation_target,
+    ));
+
+    let bridge_cluster = cluster_gene();
+
+    let bridge_hox = HoxGene {
+        placement_target: HoxPlacement::HoxGene {
+            hox_gene: HoxGeneIndex(0),
+            target_neuron: initial_target_neuron_on_mutation_target,
+        },
+        cluster_gene: ClusterGeneIndex(2),
+        disabled_connections: Vec::new(),
+    };
+
+    let expected_genome = base_genome
+        .add_cluster_gene(bridge_cluster.clone())
+        .add_hox_gene(hox_placed_on_hox_gene(
+            ClusterGeneIndex(1),
+            HoxGeneIndex(3),
+            new_target_neuron(),
+        ))
+        .add_hox_gene(bridge_hox);
+
+    let mutation = Mutation::Bridge {
+        target_hox_gene: HoxGeneIndex(1),
+        bridge_cluster_gene: bridge_cluster,
+    };
+
+    test_mutation_application(MutationApplicationTestParameters {
+        genome,
+        expected_genome,
+        mutation,
+        result_test_fn: Result::is_ok,
+    });
 }
 
 #[test]
