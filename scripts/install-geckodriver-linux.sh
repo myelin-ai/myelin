1cb51e35bdae0ef64b30e1e1d5e3911f6f2a0517
Ruben Schmidmeister <ruben@myelin.ch>
2019-09-02 13:34:24 +0200

    Fix geckodriver installation

--- scripts/install-geckodriver-linux.sh
+++ scripts/install-geckodriver-linux.sh
@@ -5,6 +5,8 @@ archive_file="geckodriver-$version-linux64.tar.gz"
 download_url="https://github.com/mozilla/geckodriver/releases/download/$version/$archive_file"
 temp_directory=$(mktemp -d)
 
-wget -O "$temp_directory/$archive_file" "$download_url"
-tar -xf "$temp_directory/$archive_file"
-sudo cp "$temp_directory/geckodriver" /usr/local/bin/
+cd "$temp_directory"
+
+wget $download_url
+tar -xf $archive_file
+sudo cp geckodriver /usr/local/bin/
