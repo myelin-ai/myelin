a7b4297921133b1b4467aed9c56523435bbd2a8d
Jeremy Stucki <jeremy@myelin.ch>
2019-09-09 11:38:00 +0200

    Use pre-built tarpaulin

---
+++ scripts/install-tarpaulin.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+version=0.8.6
+archive_name="cargo-tarpaulin-$version-travis"
+archive_file="$archive_name.tar.gz"
+download_url="https://github.com/xd009642/tarpaulin/releases/download/$version/$archive_file"
+temp_directory=$(mktemp -d)
+
+cd "$temp_directory"
+
+wget "$download_url"
+tar -xf "$archive_file"
+sudo cp "cargo-tarpaulin" /usr/local/bin/
